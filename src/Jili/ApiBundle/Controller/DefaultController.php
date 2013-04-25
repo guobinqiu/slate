a8638e830aaf0508bc1ed108d07dc5f32c83617c
lynn415 <1098078255@qq.com>
2013-04-25 16:00:47 +0800

    db find

--- src/Jili/ApiBundle/Controller/DefaultController.php
+++ src/Jili/ApiBundle/Controller/DefaultController.php
@@ -1,13 +1,19 @@
 <?php
-
 namespace Jili\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('JiliApiBundle:Default:index.html.twig', array('name' => $name));
+    	$em = $this->getDoctrine()->getManager();
+
+    	$query = $em->getRepository('JiliApiBundle:Advertiserment');
+
+    	$advertise = $query->findAll();
+        $arr['advertise'] = $advertise;
+        return $this->render('JiliApiBundle:Default:index.html.twig',$arr);
     }
 }
