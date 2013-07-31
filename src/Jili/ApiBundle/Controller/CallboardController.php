f8c907f16f4642856762f134d1aef4dfa927a837
leeqinm <278583642@qq.com>
2013-07-31 15:37:44 +0800

    对应 issue #131

--- src/Jili/ApiBundle/Controller/CallboardController.php
+++ src/Jili/ApiBundle/Controller/CallboardController.php
@@ -30,7 +30,10 @@ class  CallboardController extends Controller
     public function infoAction($id){
     	$em = $this->getDoctrine()->getManager();
     	$callboard = $em->getRepository('JiliApiBundle:Callboard')->find($id);
-    	$arr['callboard'] = $callboard;
+        if($callboard)
+    	    $arr['callboard'] = $callboard;
+        else
+            return $this->redirect($this->generateUrl('_default_error'));
     	return $this->render('JiliApiBundle:Callboard:info.html.twig',$arr);
     	
     }
