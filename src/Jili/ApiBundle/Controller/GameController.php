fc06859d0c42b555a7dd66f92b02e54f9c03d7a6
leeqinm <278583642@qq.com>
2013-08-28 14:13:27 +0800

    对应 issue #176

--- src/Jili/ApiBundle/Controller/GameController.php
+++ src/Jili/ApiBundle/Controller/GameController.php
@@ -14,8 +14,7 @@ class GameController extends Controller
 	/**
      * @Route("/index", name="_game_index")
      */
-    public function indexAction(){
-       
+    public function indexAction(){  
         return $this->render('JiliApiBundle:Game:index.html.twig');
     }
 
@@ -23,8 +22,11 @@ class GameController extends Controller
      * @Route("/chick", name="_game_chick")
      */
     public function chickAction(){
-        $code = $this->container->getParameter('init');
+        $code = '';
         $arr['code'] = $code;
+        if($this->checkMobile()!='pc'){
+            $arr['code'] = $this->container->getParameter('init_one');
+        }
         $em = $this->getDoctrine()->getManager();
         $uid = '';
         $uid = $this->get('request')->getSession()->get('uid');
@@ -39,5 +41,20 @@ class GameController extends Controller
         return $this->render('JiliApiBundle:Game:chick.html.twig',$arr);
     }
       
+    private function checkMobile(){
+        $agent = strtolower($_SERVER['HTTP_USER_AGENT']);  
+        $is_pc = (strpos($agent, 'windows nt')) ? true : false;   
+        $is_iphone = (strpos($agent, 'iphone')) ? true : false;
+        $is_ipad = (strpos($agent, 'ipad')) ? true : false;   
+        $is_android = (strpos($agent, 'android')) ? true : false; 
+        if($is_pc)
+            return 'pc';
+        if($is_iphone)
+            return 'iphone';
+        if($is_ipad) 
+            return 'ipad'; 
+        if($is_android)  
+            return 'android';
+    }
     
 }
