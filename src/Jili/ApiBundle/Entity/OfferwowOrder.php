ca18d009009e73034c4a36958c8e543944ad756a
Jarod Chiang <chiangtor@gmail.com>
2014-01-08 09:40:09 +0800

    completed issue #246

---
+++ src/Jili/ApiBundle/Entity/OfferwowOrder.php
@@ -0,0 +1,275 @@
+<?php
+
+namespace Jili\ApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * OfferwowOrder
+ *
+ * @ORM\Table(name="offerwow_order")
+ * @ORM\Entity
+ */
+class OfferwowOrder
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="user_id", type="integer", nullable=false)
+     */
+    private $userid;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=false)
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="returned_at", type="datetime", nullable=true)
+     */
+    private $returnedAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="confirmed_at", type="datetime", nullable=true)
+     */
+    private $confirmedAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="happened_at", type="datetime", nullable=true)
+     */
+    private $happenedAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="eventid", type="string", length=100, nullable=true)
+     */
+    private $eventid;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="status", type="integer", nullable=false)
+     */
+    private $status;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="delete_flag", type="integer", nullable=false)
+     */
+    private $deleteFlag;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+
+
+    /**
+     * Set userid
+     *
+     * @param integer $userid
+     * @return OfferwowOrder
+     */
+    public function setUserId($userid)
+    {
+        $this->userid = $userid;
+
+        return $this;
+    }
+
+    /**
+     * Get userid
+     *
+     * @return integer 
+     */
+    public function getUserId()
+    {
+        return $this->userid;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return OfferwowOrder
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set returnedAt
+     *
+     * @param \DateTime $returnedAt
+     * @return OfferwowOrder
+     */
+    public function setReturnedAt($returnedAt)
+    {
+        $this->returnedAt = $returnedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get returnedAt
+     *
+     * @return \DateTime 
+     */
+    public function getReturnedAt()
+    {
+        return $this->returnedAt;
+    }
+
+    /**
+     * Set confirmedAt
+     *
+     * @param \DateTime $confirmedAt
+     * @return OfferwowOrder
+     */
+    public function setConfirmedAt($confirmedAt)
+    {
+        $this->confirmedAt = $confirmedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get confirmedAt
+     *
+     * @return \DateTime 
+     */
+    public function getConfirmedAt()
+    {
+        return $this->confirmedAt;
+    }
+
+    /**
+     * Set happenedAt
+     *
+     * @param \DateTime $happenedAt
+     * @return OfferwowOrder
+     */
+    public function setHappenedAt($happenedAt)
+    {
+        $this->happenedAt = $happenedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get happenedAt
+     *
+     * @return \DateTime 
+     */
+    public function getHappenedAt()
+    {
+        return $this->happenedAt;
+    }
+
+    /**
+     * Set eventid
+     *
+     * @param string $eventid
+     * @return OfferwowOrder
+     */
+    public function setEventid($eventid)
+    {
+        $this->eventid = $eventid;
+
+        return $this;
+    }
+
+    /**
+     * Get eventid
+     *
+     * @return string 
+     */
+    public function getEventid()
+    {
+        return $this->eventid;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     * @return OfferwowOrder
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return integer 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set deleteFlag
+     *
+     * @param integer $deleteFlag
+     * @return OfferwowOrder
+     */
+    public function setDeleteFlag($deleteFlag)
+    {
+        $this->deleteFlag = $deleteFlag;
+
+        return $this;
+    }
+
+    /**
+     * Get deleteFlag
+     *
+     * @return integer 
+     */
+    public function getDeleteFlag()
+    {
+        return $this->deleteFlag;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
---
+++ src/Jili/ApiBundle/EventListener/OfferwowApiLogger.php
@@ -0,0 +1,43 @@
+<?php
+namespace Jili\ApiBundle\EventListener;
+
+use Symfony\Bridge\Monolog\Logger;
+use Doctrine\ORM\EntityManager;
+
+use Jili\ApiBundle\Entity\OfferwowApiReturn;
+
+/**
+ * 
+ **/
+class OfferwowApiLogger
+{
+
+    private $em;
+
+    private $logger;
+    public function __construct(Logger $logger , $em )
+    {
+
+        $this->logger = $logger;
+        $this->em = $em;
+
+    //    //$this->em = $em;
+    }
+
+    /**
+     *
+     * @param  $content the request uri of Adw
+     *
+     */
+    public function log($content) {
+        $adwapi = new OfferwowApiReturn();
+        $adwapi->setContent( $content);
+        $adwapi->setCreatedAt( date_create() );
+        $this->em->persist($adwapi);
+        return $this->em->flush();
+    }
+
+}
+
+
+
