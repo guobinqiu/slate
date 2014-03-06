786b1254464f4332a5116d712681915c81f360a0
zhangmm <zhangmm@voyagegroup.com.cn>
2014-03-06 17:22:00 +0800

    对应 issue #312
    手机费，亚马逊，支付宝 积分兑换成功后的数据导入优化

--- src/Jili/ApiBundle/Controller/AdminController.php
+++ src/Jili/ApiBundle/Controller/AdminController.php
@@ -1758,6 +1758,7 @@ class AdminController extends Controller
      */
     public function exchangeInAction()
     {
+        set_time_limit(1800);
         if($this->getAdminIp())
             return $this->redirect($this->generateUrl('_default_error'));
         $success = '';
