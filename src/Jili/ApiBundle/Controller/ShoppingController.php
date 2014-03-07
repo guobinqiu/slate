a25d6622afaea48e4e898642506de6f3d09f5751
Jarod Chiang <chiangtor@gmail.com>
2014-03-07 17:14:26 +0800

    fixed issue #306

--- src/Jili/ApiBundle/Controller/ShoppingController.php
+++ src/Jili/ApiBundle/Controller/ShoppingController.php
@@ -17,6 +17,8 @@ class ShoppingController extends Controller
 	 */
 	public function listAction($id=0)
 	{
+        $logger = $this->get('logger');
+        $logger->debug('{jarod}'.implode(':', array( __CLASS__, __LINE__, '')  ));
 		$reward_multiple = '';
 		$str = '';
 		$uid = '';
@@ -28,12 +30,21 @@ class ShoppingController extends Controller
 			$user = $em->getRepository('JiliApiBundle:User')->find($uid);
         	$reward_multiple = $user->getRewardMultiple();
 		}
+
 		$repository = $em->getRepository('JiliApiBundle:Advertiserment');
 		$advertise = $repository->getAdvertiserAreaList($this->container->getParameter('init_five'));
-		$adverRecommand = $repository->getAdvertiserAreaList($this->container->getParameter('init_four'));
+
+		$page_no = $this->get('request')->query->get('page', 1);
+        $page_size = 25;
+
+        $ad_ids=array();
+
+
 		foreach ( $advertise as $key=>$value){
-			$adnum = $em->getRepository('JiliApiBundle:AdwOrder')->getOrderNum($value['id']);
-			$advertise[$key]['num'] = $adnum;
+            $ad_ids[] = $value['id'];
+			#$adnum = $em->getRepository('JiliApiBundle:AdwOrder')->getOrderNum($value['id']);
+            #$advertise[$key]['num'] = $adnum;
+
             if($reward_multiple){
                 if($value['incentiveType']==2){
                     $cps_rate = $reward_multiple > $campaign_multiple ? $reward_multiple : $campaign_multiple;
@@ -47,31 +58,33 @@ class ShoppingController extends Controller
                 }
             }
 		}
-		foreach ( $adverRecommand as $key=>$value){
-            if($reward_multiple){
-                if($value['incentiveType']==2){
-                    $cps_rate = $reward_multiple > $campaign_multiple ? $reward_multiple : $campaign_multiple;
-                    $adverRecommand[$key]['reward_rate'] = $value['incentiveRate'] * $value['rewardRate'] * $cps_rate;
-                    $adverRecommand[$key]['reward_rate'] = round($adverRecommand[$key]['reward_rate']/10000,2);
-                }
-            }else{
-                if($value['incentiveType']==2){
-                    $adverRecommand[$key]['reward_rate'] = $value['incentiveRate'] * $value['rewardRate'] * $campaign_multiple;
-                    $adverRecommand[$key]['reward_rate'] = round($adverRecommand[$key]['reward_rate']/10000,2);
 
-                }
-            }
-		}
-		$arr['adverRecommand'] = $adverRecommand;
+
+        if(count($ad_ids) > 0) {
+            $ad_ids_slice = array_slice( $ad_ids, ($page_no -1 ) * $page_size , $page_size    );
+            $logger->debug('{jarod}'.implode(':', array( __CLASS__, __LINE__, '')  ).var_export( implode(',',$ad_ids_slice), true) );
+
+            $count_of_ad_joined = $em->getRepository('JiliApiBundle:AdwOrder')
+                ->getCountOfJoinedByCat( $ad_ids_slice );
+        }  else {
+            $count_of_ad_joined = array();
+        }
+
+        $arr['count_of_ad_joined']  = $count_of_ad_joined;
 		
 		$nowCate = $em->getRepository('JiliApiBundle:Advertiserment')->getAdvCate();
+
+        $arrType =array();
 		foreach ($nowCate as $key => $value) {
         	$arrType[] = $value['type'];
         }
+
         foreach ($arrType as  $value) {
         	$str .= $value.',';
         }
+
         $allCate = explode(",",$str);
+
         if($id){
         	if(!in_array($id,$allCate)){
         		return $this->redirect($this->generateUrl('_default_error'));
@@ -83,20 +96,25 @@ class ShoppingController extends Controller
 				}
 			}
 		}
+
         foreach ($actCate as $key => $value) {
     		if(!in_array($value->getId(),$allCate)){
     			unset($actCate[$key]);
     		}
     	}
     	$arr['cate'] = $actCate;
-		$arr['advertiserment'] = $advertise;
+
+		#$arr['advertiserment'] = $advertise;
+
 		$paginator  = $this->get('knp_paginator');
-		$arr['pagination'] = $paginator->paginate(
-				$advertise,
-				$this->get('request')->query->get('page', 1),
-				25
-		);
+
+        $arr['pagination'] = $paginator->paginate(
+            $advertise,
+            $page_no,
+            $page_size	
+        );
 		$arr['pagination']->setTemplate('JiliApiBundle::pagination.html.twig');
+
 		$arr['uid'] = $uid;
 		$arr['id'] = $id;
 		return $this->render('JiliApiBundle:Advertiserment:shoppinglist.html.twig',$arr);
@@ -122,37 +140,41 @@ class ShoppingController extends Controller
 		return new Response($new_url);
 	}
 	
-}
-	
+	/**
+	 * @Route("/recommend", name="_shopping_recommend")
+	 */
+    public function recommendAction() {
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+        $em = $this->getDoctrine()->getManager();
+		$uid = $this->get('request')->getSession()->get('uid');
+		$campaign_multiple = $this->container->getParameter('campaign_multiple');
+
+		if($uid){
+			$user = $em->getRepository('JiliApiBundle:User')->find($uid);
+        	$reward_multiple = $user->getRewardMultiple();
+        } else {
+            $reward_multiple = '';
+        }
+
+		$adverRecommand = $em->getRepository('JiliApiBundle:Advertiserment')->getAdvertiserAreaList($this->container->getParameter('init_four'));
+
+		foreach ( $adverRecommand as $key => $value){
+            if($reward_multiple){
+                if($value['incentiveType']==2){
+                    $cps_rate = $reward_multiple > $campaign_multiple ? $reward_multiple : $campaign_multiple;
+                    $adverRecommand[$key]['reward_rate'] = $value['incentiveRate'] * $value['rewardRate'] * $cps_rate;
+                    $adverRecommand[$key]['reward_rate'] = round($adverRecommand[$key]['reward_rate']/10000,2);
+                }
+            }else{
+                if($value['incentiveType']==2){
+                    $adverRecommand[$key]['reward_rate'] = $value['incentiveRate'] * $value['rewardRate'] * $campaign_multiple;
+                    $adverRecommand[$key]['reward_rate'] = round($adverRecommand[$key]['reward_rate']/10000,2);
+
+                }
+            }
+		}
+
+		return $this->render('JiliApiBundle:Advertiserment:recommend.html.twig',array('adverRecommand' => $adverRecommand));
+
+    }
+}
