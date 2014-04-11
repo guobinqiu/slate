466e64fd96ee57c53e409b2ac2fe62c020c475a6
zhangmm <zhangmm@voyagegroup.com.cn>
2014-04-11 17:29:38 +0800

    对应 issue #329
    接口的参数改变

--- src/Jili/ApiBundle/Controller/WenwenController.php
+++ src/Jili/ApiBundle/Controller/WenwenController.php
@@ -24,21 +24,11 @@ class WenwenController extends Controller {
 	 */
 	public function registerAction() {
 		$em = $this->getDoctrine()->getManager();
-		$token = $this->get('request')->get('secret_token');
+		$email = $this->get('request')->get('email');
+		$signature = $this->get('request')->get('signature');
+		$uniqkey = $this->get('request')->get('uniqkey');
 
-		$email = "";
-		$signature = "";
-		$uniqkey = "";
-		$params = json_decode(base64_decode(strtr($token, '-_', '+/')));
-		if ($params) {
-			$email = $params->email;
-			$signature = $params->signature;
-			if (isset ($params->uniqkey)) {
-				$uniqkey = $params->uniqkey;
-			}
-		}
-
-		$result = $this->check($email, $signature);
+		$result = $this->check($email, $signature, $uniqkey);
 		if ($result['status'] != 1) {
 			$resp = new Response(json_encode($result));
 			$resp->headers->set('Content-Type', 'text/plain');
@@ -47,15 +37,12 @@ class WenwenController extends Controller {
 
 		//存db
 		$user = new User();
-		//$user->setNick();
 		$user->setEmail($email);
 		$user->setPoints(0);
 		$user->setIsInfoSet(0);
 		$user->setRewardMultiple(1);
 		$user->setIsFromWenwen(2); //和91问问同时注册
-		if ($uniqkey) {
-			$user->setUniqkey($uniqkey);
-		}
+		$user->setUniqkey($uniqkey);
 		$em->persist($user);
 		$em->flush();
 		$str = 'jilifirstregister';
@@ -83,23 +70,41 @@ class WenwenController extends Controller {
 
 		$logger = $this->get('logger');
 		$logger->info('{WenwenController:registerAction}' . json_encode($result));
+
 		$resp = new Response(json_encode($result));
 		$resp->headers->set('Content-Type', 'text/plain');
 		return $resp;
 
 	}
 
-	private function check($email, $signature) {
+	private function check($email, $signature, $uniqkey) {
 		$result['status'] = 1;
-		if (!$email || !$signature) {
+
+		//email is null
+		if (!$email) {
 			$result['status'] = '0';
-			$result['message'] = 'secret token error';
+			$result['message'] = 'missing email';
 			return $result;
 		}
 
+		//signature is null
+		if (!$signature) {
+			$result['status'] = '0';
+			$result['message'] = 'missing signature';
+			return $result;
+		}
+
+		//uniqkey is null
+		if (!$uniqkey) {
+			$result['status'] = '0';
+			$result['message'] = 'missing uniqkey';
+			return $result;
+		}
+
+		//signature error
 		if ($this->getToken($email) != $signature) {
 			$result['status'] = '0';
-			$result['message'] = 'secret token error';
+			$result['message'] = 'access error ';
 			return $result;
 		}
 
