b70cba20d79f33501dd23f9da0e2b67a5da27844
Jarod Chiang <chiangtor@gmail.com>
2014-04-28 17:31:52 +0800

    updated next() in ghs on top partial

--- src/Jili/EmarBundle/Controller/GhsController.php
+++ src/Jili/EmarBundle/Controller/GhsController.php
@@ -14,23 +14,66 @@ use Jili\EmarBundle\Api2\Repository\GhsCat as GhsCatRepository;
 class GhsController extends Controller
 {
     /**
-     * @param: tmpl: the template to embed, max: number of records, p: the page
+     * @param: tmpl: the template to embed, 
+     * @param: max: number of records per page,
+     * @param:  p: the page number
      * @Route("/promotion/{tmpl}/{max}/{p}", defaults={"tmpl"="top", "max"=6 ,"p"=1}, requirements={"tmpl"="search|top", "max"="\d+", "p"="\d+"})
-     * @Method("GET")
-     * @Template()
+     * @Method({ "GET", "POST"})
+     * @Template
      */
     public function promotionAction($tmpl, $max, $p) {
         $request = $this->get('request');
         $logger = $this->get('logger');
         $listRequest = $this->get('ghs.list_get');
+
+        // store the max in session ? 
+        $session = $request->getSession() ;
+
+//        $session_key = 'ghs.list_get.'.$tmpl.'.fetched';
+
+//       if( $session->has('ghs.list_get.total')) {
+//          $total = 
+//        } else {
+        
+#        $listRequest->setPageSize(1);
+#        $params = array('page_no' => 1);
+#        $listRequest->setApp('search')->fetchDistinct( $params );
+#        $total = $listRequest->getTotal();
+
+
+#        $last_page =ceil( $total/$max);
+        
+        // share the data in cache file.
+        $last_page = 58;
+
+        if( $p > $last_page) {
+            $page = $p % $last_page;
+        } else {
+            if( $p > 0 ) {
+                $page  = $p;
+            } else {
+                $page = mt_rand(1, $last_page);
+                // how to escape the duplicate ones ?? 
+            }
+        }
+
+   //     }
+
+#        $logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,'total','')) . var_export( $total, true));
+        $logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,'last_page','')) . var_export( $last_page, true));
+        $logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,'page','')) . var_export( $page, true));
+
         // multiple by 2 to filter the unecessary links. 
         // NOTICE: always fetch the first page ?
+
         $listRequest->setPageSize($max );
-        $params = array('page_no' => $p);
+        $params = array('page_no' => $page);
+
+        $logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,'params','')) . var_export( $params, true));
+        $logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,'total2','')) . var_export( $listRequest->getTotal() , true));
         $uid = $request->getSession()->get('uid');
         $list = $listRequest->setApp('search')->fetchDistinct( $params );
-        $total = $listRequest->getTotal();
-        #$logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,'list','')) . var_export( $list, true));
+
         if( $request->isXmlHttpRequest()) {
             $prds = array();
             foreach( $list as $v) {
@@ -42,6 +85,7 @@ class GhsController extends Controller
                     'dis'=> round( $v['discount'] *  $this->container->getParameter('emar_com.cps.action.default_rebate')/100, 2),
                     'buy'=>$v['bought'] ); 
             }
+
 #             $logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,'prds','')) . var_export( $prds, true));
             $response = new Response(json_encode(array('prds' => $prds)));
             $response->headers->set('Content-Type', 'application/json');
@@ -49,7 +93,7 @@ class GhsController extends Controller
         }
 
         $template ='JiliEmarBundle:Ghs:'. 'promotion_on_'. $tmpl. '.html.twig';
-        return $this->render($template, array('ghs_pdts'=> $list ));
+        return $this->render($template, array('ghs_pdts'=> $list ,'page'=> $page,'last_page'=>$last_page));
     }
 
     /**
