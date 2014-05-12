fcba6df88f0a3a1930227aaf36dfb5d19fc46ac0
Jarod Chiang <chiangtor@gmail.com>
2014-05-12 10:25:28 +0800

    #350 added

--- src/Jili/EmarBundle/EventListener/ProductCategories.php
+++ src/Jili/EmarBundle/EventListener/ProductCategories.php
@@ -3,18 +3,15 @@ namespace Jili\EmarBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Log\LoggerInterface;
 
-use Symfony\Component\Filesystem\Filesystem;
-
 use Jili\EmarBundle\Api2\Repository\ItemCat as ItemCatRepository,
   Jili\EmarBundle\Api2\Repository\WebCat as WebCatRepository,
   Jili\EmarBundle\Api2\Repository\WebList as WebListRepository;
 
 class ProductCategories {
-
     private $logger;
-
     private $generalCategoryGet;
-
+    private $cache_fn = 'emar.product_categories';
+    private $duration = 90;
     /**
      * 1. fetch first level category,
      * 2. fetch second level category based on first level category;
@@ -22,41 +19,39 @@ class ProductCategories {
      * $force always do remote request when is set ture, in another words, if force is false the cache accessing is allowed.
      */
     public function fetch( $force = false ) {
-
-        $cached = $this->cache_dir.DIRECTORY_SEPARATOR.'emar_product_category_'.date('Ym').'.cached';
-
-        $fs = new Filesystem();
-
-        if(! $force &&  $fs->exists($cached) ) {
-            $prod_categories = @unserialize(file_get_contents($cached));
-        }
+        $prod_categories = null ;
+        if($this->cache_file_handler->isValid($this->cache_fn , $this->duration) ) {
+            $prod_categories = $this->cache_file_handler->get($this->cache_fn);
+#           $this->logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,'')) . var_export( $cats, true));
+        } 
 
         if( !isset($prod_categories) || ! is_array($prod_categories) || ! isset($prod_categories['cats']) || ! isset($prod_categories['sub_cats']))  {
+
+           $this->logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,'')) . var_export( $prod_categories, true));
             // cats
             $categories_raw  = $this->generalCategoryGet->fetch();
-
             $cats = ItemCatRepository::parse( $categories_raw);
-
             #$this->logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,'')) . var_export( $cats, true));
-
             $sub_cats = array();
-
             foreach( $categories_raw as $cat ) {
-
                 $cid = $cat['catid'];
-
                 $params = array('parent_id' => $cid);
-
                 $sub_cats_raw = $this->generalCategoryGet->fetch($params);
-
                 $sub_cats[ $cid ] =  ItemCatRepository::parse( $sub_cats_raw);
-
                 #$this->logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,'')) . var_export( $params, true));
             }
 
             $prod_categories = compact('cats', 'sub_cats');
+            $this->logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,'')) . var_export( $this->cache_fn , true));
+            
+            $this->cache_file_handler->remove($this->cache_fn );
+            $this->cache_file_handler->set($this->cache_fn , $prod_categories);
+            //@file_put_contents( $cached, serialize($prod_categories) , LOCK_EX);
+            //
+            $this->logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,' cache not used')) );
+        } else {
+            $this->logger->debug('{jarod}'.implode( ':', array(__CLASS__ , __LINE__,' cache used')) );
 
-            @file_put_contents( $cached, serialize($prod_categories) , LOCK_EX);
         }
         return $prod_categories;
     }
@@ -73,6 +68,10 @@ class ProductCategories {
 
     public function setGeneralCategoryGet(  $getter ) {
         $this->generalCategoryGet= $getter ;
+    }
+    public function setCacheFileHandler( $handler)
+    {
+        $this->cache_file_handler = $handler;
     }
 
 }
