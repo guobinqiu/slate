46938869344d0b4f7af74c36063a617f2c635a5a
zhangmm <zhangmm@voyagegroup.com.cn>
2014-08-28 17:06:25 +0800

    #437 快速问答接口
    用symfony 中 Command 方式实现对接口数据的保存

---
+++ src/Jili/ApiBundle/Command/VoteApiCommand.php
@@ -0,0 +1,50 @@
+<?php
+namespace Jili\ApiBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class VoteApiCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('jili:vote_api')->setDescription('get vote data from 91wenwen');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('start...');
+        $output_filename = $this->getContainer()->getParameter('file_path_wenwen_vote');
+        $url = $this->getContainer()->getParameter('wenwen_vote_api');
+
+        //请求api接口
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $return = curl_exec($ch);
+        curl_close($ch);
+
+        //解析接口数据
+        $data = json_decode($return, true);
+
+        if ($data['meta'] && $data['meta']['code'] == 200) {
+
+            $votes = $data['data'];
+
+            //保存接口数据
+            $handle = fopen($output_filename, 'w');
+            if ($handle) {
+                fputcsv($handle, $votes[count($votes) - 1]);
+            }
+            fclose($handle);
+
+        }
+        $output->writeln('successfully');
+    }
+}
