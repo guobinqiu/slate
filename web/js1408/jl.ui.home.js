41b21f3ff3d5d07d53b177a42d14715d566b623c
Amy Zhao <amy_zhao@researchpanelasia.com>
2014-08-28 11:50:33 +0800

    fix css and js

--- web/js1408/jl.ui.home.js
+++ web/js1408/jl.ui.home.js
@@ -144,3 +144,21 @@
     });
 })(jQuery);
 
+(function($){
+	//截取字符串长度
+		jQuery.fn.limit=function(){ 
+			var self = $("div[limit]"); 
+			self.each(function(){ 
+				var objString = $(this).text(); 
+				var objLength = $(this).text().length; 
+				var num = $(this).attr("limit"); 
+				if(objLength > num){ 
+		$(this).attr("title",objString); 
+					objString = $(this).text(objString.substring(0,num) + "..."); 
+				} 
+			}) 
+		} 
+		$(function(){ 
+			$(document.body).limit(); 
+		}) 
+})(jQuery);
