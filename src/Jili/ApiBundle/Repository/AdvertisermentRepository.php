76206eff1d10ec1643ded0ebbd9cc8a93dc85f2c
Jarod Chiang <chiangtor@gmail.com>
2014-10-08 08:49:50 +0800

    #469

--- src/Jili/ApiBundle/Repository/AdvertisermentRepository.php
+++ src/Jili/ApiBundle/Repository/AdvertisermentRepository.php
@@ -219,6 +219,7 @@ class AdvertisermentRepository extends EntityRepository
 
 
     /**
+     * @abstract 
      * @param  integer $uid the user.id 
      * @param integer $aid the advertiserment.id
      * @return  adversiterment target url
@@ -230,33 +231,19 @@ class AdvertisermentRepository extends EntityRepository
         $adw_info = $advertiserment->getImageurl();
         $adw_info = explode('u=',$adw_info);
         $new_url = trim($adw_info[0]).'u='.$uid.trim($adw_info[1]).$aid;
-        return trim($new_url);
-    }
-    
-    /**
-     * @abstract The response of curl request must match pattern 
-     *              "^<script>window.location.href='http://%s';</script>$"
-     * @param  integer $uid the user.id 
-     * @param integer $aid the advertiserment.id
-     */
-    public function getRedirect($uid,$aid)
-    {
-        $url = $this->advInfo($uid, $aid);
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $return = curl_exec($ch);
-        curl_close($ch);
-
-        // adid=33
-        // <script>window.location.href=\'http://union.dangdang.com/transfer/transfer.aspx?from=430-88295510876&backurl=http://book.dangdang.com\';</script>
-
-        if( substr($url, -9 )  === '</script>' && substr($url, 0,30 ) === '<script>window.location.href=' ) {
-
+        if($advertiserment->isScriptRedirect()) {
+            // The response of curl request must match pattern "^<script>window.location.href='http://%s';</script>$"
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $return = curl_exec($ch);
+            curl_close($ch);
+            $c = trim($return);
+            if( substr($c, -9 )  === '</script>' && substr($c, 0,30 ) === '<script>window.location.href=\'' ) {
+                $new_url= substr($c, 30, -11); 
+            }
         }
-        return $return;
-
+        return trim($new_url);
     }
 }
