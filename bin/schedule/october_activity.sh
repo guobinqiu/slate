78d8b3c7ee3bb183f6db1fc2fe8a7e56d5f22466
zhangmm <zhangmm@voyagegroup.com.cn>
2014-10-14 15:14:57 +0800

    10月活动 #495

--- bin/schedule/october_activity.sh
+++ bin/schedule/october_activity.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # crontab 
-#  bash ...../september_activity.sh &2>/tmp/a.log &1>/dev/null 
+#  bash ...../october_activity.sh &2>/tmp/a.log &1>/dev/null 
 
 cd $(dirname $0)/../../;
 ./app/console jili:cpa_ranking_activity "2014-10-01 00:00:00" "2014-10-31 23:59:59" -e prod 
