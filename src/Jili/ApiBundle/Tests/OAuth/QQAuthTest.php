095aec7cc2378b18076064ce4666038b69f4d0db
Jarod Chiang <chiangtor@gmail.com>
2014-10-20 10:34:32 +0800

    #474

---
+++ src/Jili/ApiBundle/Tests/OAuth/QQAuthTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Jili\ApiBundle\Tests\OAuth;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class QQAutoTest  extends KernelTestCase
+{
+
+    public function setUp() 
+    {
+
+    }
+    public function tearDown()
+    {
+
+    }
+
+
+}
