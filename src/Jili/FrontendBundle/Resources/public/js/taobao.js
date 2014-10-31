68d5129aefde38e8d0b40b8a27cdabbb6d4f6915
wltonline <wlt_liting@163.com>
2014-10-31 10:44:22 +0800

    修复点击按钮样式 #504

--- src/Jili/FrontendBundle/Resources/public/js/taobao.js
+++ src/Jili/FrontendBundle/Resources/public/js/taobao.js
@@ -1,55 +1,66 @@
-var jili = jili || {};
+(function($){
+	var jili = jili || {};
 
-jili.taobao = {};
-
-jili.taobao.searchBycatgory = function(id) {
-	$.ajax({
-		url: Routing.generate("jili_frontend_taobao_categoryapi", {
-			"id": id
-		}),
-		type: "GET",
-		dataType: "json",
-		success: function(obj) {
-			$(window).unbind('.infscr');
-			$(window).scrollTop(0);
-			var items = obj.keywords;
-			var $box = $('<div class="tablist block" id="tabs-1"><div id="waterfall"></div><div id="more" class="more"></div></div>');
-			var html = '';
-			for (var i = 0; i < items.length; i++) {
-				html += "<div class=\"cell\">" + items[i]['content'] + "</div>";				
-			}
-			$box.find("#waterfall").html(html);
-			$box.find("#more").html("<a href="+Routing.generate("jili_frontend_taobao_categoryapi", {"id": id,"page":2})+"></a>");
-			$('.taobaoCon').html($box);
-			$('#tabs-1').infinitescroll({
-				loading: {
-					finishedMsg: "<br style='clear:both'/><em>没有更多了。</em>",
-					img: "{{ asset('images/taobao/progressBar.gif')}}",
-					msgText: ""
-				},
-				navSelector: '#more',
-				nextSelector: '#more a',
-				itemSelector: ".cell",
-				debug: false,
-				dataType: 'json',
-				contentSelector: '#waterfall',
-				animate: false,
-				extraScrollPx: 50,
-				template: function(data) {
-					$boxes = $("<div class='container'></div>");
-					$.each(data.keywords, function(key, value)
-					{
-						myString = "<div class = 'cell'>" + value.content + "</div>";
-						$box = $(myString);
-						$boxes.append($box);
-					});
-					return $boxes.find('.cell');
+	jili.taobao = {};
+	
+	jili.taobao.searchBycatgory = function(id) {
+		$.ajax({
+			url: Routing.generate("jili_frontend_taobao_categoryapi", {
+				"id": id
+			}),
+			type: "GET",
+			dataType: "json",
+			success: function(obj) {
+				$(window).unbind('.infscr');
+				$(window).scrollTop(0);
+				var items = obj.keywords;
+				var $box = $('<div class="tablist block" id="tabs-1"><div id="waterfall"></div><div id="more" class="more"></div></div>');
+				var html = '';
+				for (var i = 0; i < items.length; i++) {
+					html += "<div class=\"cell\">" + items[i]['content'] + "</div>";				
 				}
-			})
-		},
-		error: function(e) {
-			console.log(e.message);
-		}
-	});
-}
-
+				$box.find("#waterfall").html(html);
+				$box.find("#more").html("<a href="+Routing.generate("jili_frontend_taobao_categoryapi", {"id": id,"page":2})+"></a>");
+				$('.taobaoCon').html($box);
+				$('#tabs-1').infinitescroll({
+					loading: {
+						finishedMsg: "<br style='clear:both'/><em>没有更多了。</em>",
+						img: "{{ asset('images/taobao/progressBar.gif')}}",
+						msgText: ""
+					},
+					navSelector: '#more',
+					nextSelector: '#more a',
+					itemSelector: ".cell",
+					debug: false,
+					dataType: 'json',
+					contentSelector: '#waterfall',
+					animate: false,
+					extraScrollPx: 50,
+					template: function(data) {
+						$boxes = $("<div class='container'></div>");
+						$.each(data.keywords, function(key, value)
+						{
+							myString = "<div class = 'cell'>" + value.content + "</div>";
+							$box = $(myString);
+							$boxes.append($box);
+						});
+						return $boxes.find('.cell');
+					}
+				})
+			},
+			error: function(e) {
+				console.log(e.message);
+			}
+		});
+	}
+	$(function(){
+    	jili.taobao.searchBycatgory(1); 
+		$('.ltab li:first').addClass('ui-tabs-active');
+		$('.ltab li').on('click', function(){
+			var index = $('.ltab li').index(this);
+			var categoryId = $(this).attr('id');
+			$('.ltab li').removeClass('ui-tabs-active').eq(index).addClass('ui-tabs-active');
+			jili.taobao.searchBycatgory(categoryId); 
+		});
+    });
+})(jQuery);
