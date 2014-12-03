994d06a1e5683d03030ae76c6d23a7527e81ba38
zhangmm <zhangmm@voyagegroup.com.cn>
2014-12-03 16:46:46 +0800

    #550
    从亚马逊获得米粒的用户，手动处理订单

---
+++ scripts/bug_issue550/log_file.sql
@@ -0,0 +1,64 @@
+snaille@sina.com: SELECT id,email,points FROM user WHERE email = 'snaille@sina.com'
+user_id:1073278, email: snaille@sina.com, points: 3842
+snaille@sina.com: select * from task_history08 where user_id =1073278 and point = 283 and task_name like '%亚马逊%' and date like '2014-03-13%'
+id:222084, category_type: 2, status: 2, task_name:           亚马逊           
+snaille@sina.com: update task_history08 set status = 3 where id = 222084
+snaille@sina.com: update user set points = points + 283 where id = 1073278
+snaille@sina.com: insert into point_history08 (user_id,point_change_num,reason,create_time) values (1073278,283,2,'2014-12-03 16:39:27')
+
+
+mcj19821221@163.com: SELECT id,email,points FROM user WHERE email = 'mcj19821221@163.com'
+user_id:1062467, email: mcj19821221@163.com, points: 2385
+mcj19821221@163.com: select * from task_history07 where user_id =1062467 and point = 642 and task_name like '%亚马逊%' and date like '2014-06-28%'
+id:169343, category_type: 19, status: 2, task_name:亚马逊移动CPS
+mcj19821221@163.com: update task_history07 set status = 3 where id = 169343
+mcj19821221@163.com: update user set points = points + 642 where id = 1062467
+mcj19821221@163.com: insert into point_history07 (user_id,point_change_num,reason,create_time) values (1062467,642,19,'2014-12-03 16:39:27')
+
+
+palar0186@163.com: SELECT id,email,points FROM user WHERE email = 'palar0186@163.com'
+user_id:1096963, email: palar0186@163.com, points: 151
+palar0186@163.com: select * from task_history03 where user_id =1096963 and point = 231 and task_name like '%亚马逊%' and date like '2014-07-27%'
+id:187392, category_type: 19, status: 2, task_name:亚马逊返点站CPS
+palar0186@163.com: update task_history03 set status = 3 where id = 187392
+palar0186@163.com: update user set points = points + 231 where id = 1096963
+palar0186@163.com: insert into point_history03 (user_id,point_change_num,reason,create_time) values (1096963,231,19,'2014-12-03 16:39:28')
+
+
+duzhenghang@163.com: SELECT id,email,points FROM user WHERE email = 'duzhenghang@163.com'
+user_id:1093492, email: duzhenghang@163.com, points: 222
+duzhenghang@163.com: select * from task_history02 where user_id =1093492 and point = 42 and task_name like '%亚马逊%' and date like '2014-04-24%'
+id:17857, category_type: 2, status: 2, task_name:           亚马逊           
+duzhenghang@163.com: update task_history02 set status = 3 where id = 17857
+duzhenghang@163.com: update user set points = points + 42 where id = 1093492
+duzhenghang@163.com: insert into point_history02 (user_id,point_change_num,reason,create_time) values (1093492,42,2,'2014-12-03 16:39:29')
+
+
+duzhenghang@163.com: SELECT id,email,points FROM user WHERE email = 'duzhenghang@163.com'
+user_id:1093492, email: duzhenghang@163.com, points: 222
+duzhenghang@163.com: select * from task_history02 where user_id =1093492 and point = 49 and task_name like '%亚马逊%' and date like '2014-07-28%'
+id:188958, category_type: 2, status: 2, task_name:           亚马逊           
+duzhenghang@163.com: update task_history02 set status = 3 where id = 188958
+duzhenghang@163.com: update user set points = points + 49 where id = 1093492
+duzhenghang@163.com: insert into point_history02 (user_id,point_change_num,reason,create_time) values (1093492,49,2,'2014-12-03 16:39:30')
+
+
+shzknn@sina.com: SELECT id,email,points FROM user WHERE email = 'shzknn@sina.com'
+user_id:1141938, email: shzknn@sina.com, points: 1255
+shzknn@sina.com: select * from task_history08 where user_id =1141938 and point = 52 and task_name like '%亚马逊%' and date like '2014-08-20%'
+id:209943, category_type: 19, status: 2, task_name:亚马逊返点站CPS
+shzknn@sina.com: update task_history08 set status = 3 where id = 209943
+shzknn@sina.com: update user set points = points + 52 where id = 1141938
+shzknn@sina.com: insert into point_history08 (user_id,point_change_num,reason,create_time) values (1141938,52,19,'2014-12-03 16:39:31')
+
+
+386022827@qq.com: SELECT id,email,points FROM user WHERE email = '386022827@qq.com'
+user_id:1132798, email: 386022827@qq.com, points: 603
+386022827@qq.com: select * from task_history08 where user_id =1132798 and point = 15 and task_name like '%亚马逊%' and date like '2014-07-08%'
+id:166877, category_type: 19, status: 2, task_name:亚马逊返点站CPS
+386022827@qq.com: update task_history08 set status = 3 where id = 166877
+386022827@qq.com: update user set points = points + 15 where id = 1132798
+386022827@qq.com: insert into point_history08 (user_id,point_change_num,reason,create_time) values (1132798,15,19,'2014-12-03 16:39:31')
+
+
+完成了
