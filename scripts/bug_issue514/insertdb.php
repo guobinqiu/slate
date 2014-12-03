fdc91204eccb48f41fd43575b3096107dc3cb435
zhangmm <zhangmm@voyagegroup.com.cn>
2014-12-03 11:20:05 +0800

    #514

--- scripts/bug_issue514/insertdb.php
+++ scripts/bug_issue514/insertdb.php
@@ -3,13 +3,15 @@ $log_file = "D:/xampp/htdocs/PointMedia/scripts/bug_issue514/log_file_insert.txt
 $log_handle = fopen($log_file, "a+");
 
 $link = mysql_connect('localhost', 'root', '') or die('Could not connect: ' . mysql_error());
-mysql_select_db('jili_2014-09-23-test') or die('Could not select database');
+mysql_select_db('jili_2014-09-23') or die('Could not select database');
 mysql_set_charset("UTF8", $link);
 
 mysql_query("BEGIN"); //开始一个事务
 mysql_query("SET AUTOCOMMIT=0"); //设置事务不自动commit
 
 $count = 0;
+$total = 0;
+
 for ($i = 0; $i < 10; $i++) {
     $query = "SELECT * , count( user_id ) count
                 FROM point_history0" . $i . "
@@ -31,21 +33,21 @@ for ($i = 0; $i < 10; $i++) {
         }
 
         if ($count == 4) {
-            $roolback = insertDb($log_handle, $value);
+            $roolback = insertDb($log_handle, $value, $total);
             if ($roolback) {
                 mysql_query("ROOLBACK");
                 fwrite($log_handle, $value['user_id'] . "修复失败，米粒：" . $value['point_change_num'] . "\r\n");
                 exit;
             }
 
-            $roolback = insertDb($log_handle, $value);
+            $roolback = insertDb($log_handle, $value, $total);
             if ($roolback) {
                 mysql_query("ROOLBACK");
                 fwrite($log_handle, $value['user_id'] . "修复失败，米粒：" . $value['point_change_num'] . "\r\n");
                 exit;
             }
         } else {
-            $roolback = insertDb($log_handle, $value);
+            $roolback = insertDb($log_handle, $value, $total);
             if ($roolback) {
                 mysql_query("ROOLBACK");
                 fwrite($log_handle, $value['user_id'] . "修复失败，米粒：" . $value['point_change_num'] . "\r\n");
@@ -61,14 +63,15 @@ for ($i = 0; $i < 10; $i++) {
 mysql_query("COMMIT"); //执行事务
 
 mysql_close($link);
-fwrite($log_handle, "执行完成" . "\r\n");
+fwrite($log_handle, "执行完成\r\n重复记录总数：$total" . "\r\n");
 fclose($log_handle);
 echo "执行完成!";
 exit;
 
-function insertDb($log_handle, $value) {
+function insertDb($log_handle, $value, & $total) {
 
     $roolback = false;
+    $total++;
 
     //update point_history00
     $ph_sql = "insert into point_history0" . ($value['user_id'] % 10) . " (user_id,point_change_num,reason,create_time) values (" . $value['user_id'] . "," . (- $value['point_change_num']) . ",13,'" . $value['create_time'] . "')";
