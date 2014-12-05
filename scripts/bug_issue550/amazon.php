f7ecd031c8d20bfbeba86502847c35b3bbfed051
zhangmm <zhangmm@voyagegroup.com.cn>
2014-12-05 17:54:22 +0800

    #550 使用 PDO方式

--- scripts/bug_issue550/amazon.php
+++ scripts/bug_issue550/amazon.php
@@ -11,69 +11,68 @@ if ($file_handle !== FALSE) {
 }
 fclose($file_handle);
 
-$link = mysql_connect('localhost', 'root', '') or die('Could not connect: ' . mysql_error());
-mysql_select_db('jili_2014-09-23') or die('Could not select database');
-mysql_set_charset("UTF8", $link);
+$dsn = "mysql:host=localhost;dbname=jili_2014-09-23";
+$dbh = new PDO($dsn, 'root', '');
 
-mysql_query("BEGIN"); //开始一个事务
-mysql_query("SET AUTOCOMMIT=0"); //设置事务不自动commit
+$dbh->beginTransaction();
 
-foreach ($contents as $value) {
+try {
 
-    $date = $value[0];
-    $email = $value[1];
-    $point = $value[2];
+    foreach ($contents as $value) {
 
-    $user_sql = "SELECT id,email,points FROM user WHERE email = '" . $email . "'";
-    fwrite($log_handle, $email . ": " . $user_sql . "\r\n");
-    $result = mysql_query($user_sql) or die('Query failed: ' . mysql_error());
-    $user = mysql_fetch_assoc($result);
-    fwrite($log_handle, "user_id:" . $user['id'] . ", email: " . $user['email'] . ", points: " . $user['points'] . "\r\n");
+        $date = $value[0];
+        $email = $value[1];
+        $point = $value[2];
 
-    $task_history_sql = "select * from task_history0" . ($user['id'] % 10) . " where user_id =" . $user['id'] . " and point = " . $point . " and task_name like '%亚马逊%' and date like '" . $date . "%'";
-    fwrite($log_handle, $email . ": " . $task_history_sql . "\r\n");
-    $result = mysql_query($task_history_sql) or die('Query failed: ' . mysql_error());
-    $task_history = mysql_fetch_assoc($result);
-    fwrite($log_handle, "id:" . $task_history['id'] . ", category_type: " . $task_history['category_type'] . ", status: " . $task_history['status'] . ", task_name:" . $task_history['task_name'] . "\r\n");
+        //检索user
+        $user_sql = "SELECT id,email,points FROM user WHERE email = '" . $email . "'";
+        fwrite($log_handle, $email . ": " . $user_sql . "\r\n");
+        $query = $dbh->prepare($user_sql);
+        $query->execute() or die(print_r($dbh->errorInfo(), true));
+        $user = $query->fetch(PDO :: FETCH_ASSOC);
+        fwrite($log_handle, "email: " . $user['email'] . "user_id:" . $user['id'] . " , points: " . $user['points'] . "\r\n");
 
-    //update task_history
-    $task_history_update_sql = "update task_history0" . ($user['id'] % 10) . " set status = 3 where id = " . $task_history['id'];
-    fwrite($log_handle, $email . ": " . $task_history_update_sql . "\r\n");
-//    $task_history_flag = mysql_query($task_history_update_sql);
-//    if (!$task_history_flag) {
-//        mysql_query("ROOLBACK");
-//        exit;
-//    }
+        //检索task_history
+        $task_history_sql = "select * from task_history0" . ($user['id'] % 10) . " where user_id =" . $user['id'] . " and point = " . $point . " and task_name like '%亚马逊%' and date like '" . $date . "%'";
+        fwrite($log_handle, $email . ": " . $task_history_sql . "\r\n");
+        $query = $dbh->prepare($task_history_sql);
+        $query->execute() or die(print_r($dbh->errorInfo(), true));
+        $task_history = $query->fetch(PDO :: FETCH_ASSOC);
+        fwrite($log_handle, "id:" . $task_history['id'] . ", category_type: " . $task_history['category_type'] . ", status: " . $task_history['status'] . ", task_name:" . $task_history['task_name'] . "\r\n");
 
-    //update user
-    $user_update_sql = "update user set points = points + " . $point . " where id = " . $user['id'];
-    fwrite($log_handle, $email . ": " . $user_update_sql . "\r\n");
-//    $user_flag = mysql_query($user_update_sql);
-//    if (!$user_flag) {
-//        mysql_query("ROOLBACK");
-//        exit;
-//    }
+        //update task_history
+        $task_history_update_sql = "update task_history0" . ($user['id'] % 10) . " set status = 3 where id = " . $task_history['id'];
+        fwrite($log_handle, $email . ": " . $task_history_update_sql . "\r\n");
+        //        $query = $dbh->prepare($task_history_update_sql);
+        //        $query->execute() or die(print_r($dbh->errorInfo(), true));
 
-    //insert point_history00
-    $create_time = date('Y-m-d H:i:s');
+        //update user
+        $user_update_sql = "update user set points = points + " . $point . " where id = " . $user['id'];
+        fwrite($log_handle, $email . ": " . $user_update_sql . "\r\n");
+        //        $query = $dbh->prepare($user_update_sql);
+        //        $query->execute() or die(print_r($dbh->errorInfo(), true));
 
-    $ph_sql = "insert into point_history0" . ($user['id'] % 10) . " (user_id,point_change_num,reason,create_time) values (" . $user['id'] . "," . $point . "," . $task_history['category_type'] . ",'" . $create_time . "')";
-    fwrite($log_handle, $email . ": " . $ph_sql . "\r\n");
-//    $ph_flag = mysql_query($ph_sql);
-//    if (!$ph_flag) {
-//        mysql_query("ROOLBACK");
-//        exit;
-//    }
+        //insert point_history00
+        $create_time = date('Y-m-d H:i:s');
 
-    fwrite($log_handle, "\r\n\r\n");
+        $ph_sql = "insert into point_history0" . ($user['id'] % 10) . " (user_id,point_change_num,reason,create_time) values (" . $user['id'] . "," . $point . "," . $task_history['category_type'] . ",'" . $create_time . "')";
+        fwrite($log_handle, $email . ": " . $ph_sql . "\r\n");
+        //        $query = $dbh->prepare($ph_sql);
+        //        $query->execute() or die(print_r($dbh->errorInfo(), true));
 
+        fwrite($log_handle, "\r\n\r\n");
+
+    }
+
+    $dbh->commit();
+    fwrite($log_handle, "Success!\r\n\r\n");
+
+} catch (Exception $e) {
+    $dbh->rollBack();
+    fwrite($log_handle, "Failed: " . $e->getMessage() . "\r\n\r\n");
 }
 
-mysql_query("COMMIT"); //执行事务
-
-mysql_close($link);
-fwrite($log_handle, "完成了" . "\r\n");
 fclose($log_handle);
-echo "执行完成!";
+echo "执行结束!";
 exit;
 ?>
--- scripts/bug_issue550/log_file.sql
+++ scripts/bug_issue550/log_file.sql
@@ -1,64 +1,65 @@
 snaille@sina.com: SELECT id,email,points FROM user WHERE email = 'snaille@sina.com'
-user_id:1073278, email: snaille@sina.com, points: 3842
+email: snaille@sina.com, user_id:1073278, points: 3842
 snaille@sina.com: select * from task_history08 where user_id =1073278 and point = 283 and task_name like '%亚马逊%' and date like '2014-03-13%'
-id:222084, category_type: 2, status: 2, task_name:           亚马逊           
-snaille@sina.com: update task_history08 set status = 3 where id = 222084
+id:, category_type: , status: , task_name:
+snaille@sina.com: update task_history08 set status = 3 where id = 
 snaille@sina.com: update user set points = points + 283 where id = 1073278
-snaille@sina.com: insert into point_history08 (user_id,point_change_num,reason,create_time) values (1073278,283,2,'2014-12-03 16:39:27')
+snaille@sina.com: insert into point_history08 (user_id,point_change_num,reason,create_time) values (1073278,283,,'2014-12-05 17:52:50')
 
 
 mcj19821221@163.com: SELECT id,email,points FROM user WHERE email = 'mcj19821221@163.com'
-user_id:1062467, email: mcj19821221@163.com, points: 2385
+email: mcj19821221@163.com, user_id:1062467, points: 2385
 mcj19821221@163.com: select * from task_history07 where user_id =1062467 and point = 642 and task_name like '%亚马逊%' and date like '2014-06-28%'
-id:169343, category_type: 19, status: 2, task_name:亚马逊移动CPS
-mcj19821221@163.com: update task_history07 set status = 3 where id = 169343
+id:, category_type: , status: , task_name:
+mcj19821221@163.com: update task_history07 set status = 3 where id = 
 mcj19821221@163.com: update user set points = points + 642 where id = 1062467
-mcj19821221@163.com: insert into point_history07 (user_id,point_change_num,reason,create_time) values (1062467,642,19,'2014-12-03 16:39:27')
+mcj19821221@163.com: insert into point_history07 (user_id,point_change_num,reason,create_time) values (1062467,642,,'2014-12-05 17:52:52')
 
 
 palar0186@163.com: SELECT id,email,points FROM user WHERE email = 'palar0186@163.com'
-user_id:1096963, email: palar0186@163.com, points: 151
+email: palar0186@163.com, user_id:1096963, points: 151
 palar0186@163.com: select * from task_history03 where user_id =1096963 and point = 231 and task_name like '%亚马逊%' and date like '2014-07-27%'
-id:187392, category_type: 19, status: 2, task_name:亚马逊返点站CPS
-palar0186@163.com: update task_history03 set status = 3 where id = 187392
+id:, category_type: , status: , task_name:
+palar0186@163.com: update task_history03 set status = 3 where id = 
 palar0186@163.com: update user set points = points + 231 where id = 1096963
-palar0186@163.com: insert into point_history03 (user_id,point_change_num,reason,create_time) values (1096963,231,19,'2014-12-03 16:39:28')
+palar0186@163.com: insert into point_history03 (user_id,point_change_num,reason,create_time) values (1096963,231,,'2014-12-05 17:52:53')
 
 
 duzhenghang@163.com: SELECT id,email,points FROM user WHERE email = 'duzhenghang@163.com'
-user_id:1093492, email: duzhenghang@163.com, points: 222
+email: duzhenghang@163.com, user_id:1093492, points: 222
 duzhenghang@163.com: select * from task_history02 where user_id =1093492 and point = 42 and task_name like '%亚马逊%' and date like '2014-04-24%'
-id:17857, category_type: 2, status: 2, task_name:           亚马逊           
-duzhenghang@163.com: update task_history02 set status = 3 where id = 17857
+id:, category_type: , status: , task_name:
+duzhenghang@163.com: update task_history02 set status = 3 where id = 
 duzhenghang@163.com: update user set points = points + 42 where id = 1093492
-duzhenghang@163.com: insert into point_history02 (user_id,point_change_num,reason,create_time) values (1093492,42,2,'2014-12-03 16:39:29')
+duzhenghang@163.com: insert into point_history02 (user_id,point_change_num,reason,create_time) values (1093492,42,,'2014-12-05 17:52:54')
 
 
 duzhenghang@163.com: SELECT id,email,points FROM user WHERE email = 'duzhenghang@163.com'
-user_id:1093492, email: duzhenghang@163.com, points: 222
+email: duzhenghang@163.com, user_id:1093492, points: 222
 duzhenghang@163.com: select * from task_history02 where user_id =1093492 and point = 49 and task_name like '%亚马逊%' and date like '2014-07-28%'
-id:188958, category_type: 2, status: 2, task_name:           亚马逊           
-duzhenghang@163.com: update task_history02 set status = 3 where id = 188958
+id:, category_type: , status: , task_name:
+duzhenghang@163.com: update task_history02 set status = 3 where id = 
 duzhenghang@163.com: update user set points = points + 49 where id = 1093492
-duzhenghang@163.com: insert into point_history02 (user_id,point_change_num,reason,create_time) values (1093492,49,2,'2014-12-03 16:39:30')
+duzhenghang@163.com: insert into point_history02 (user_id,point_change_num,reason,create_time) values (1093492,49,,'2014-12-05 17:52:55')
 
 
 shzknn@sina.com: SELECT id,email,points FROM user WHERE email = 'shzknn@sina.com'
-user_id:1141938, email: shzknn@sina.com, points: 1255
+email: shzknn@sina.com, user_id:1141938, points: 1255
 shzknn@sina.com: select * from task_history08 where user_id =1141938 and point = 52 and task_name like '%亚马逊%' and date like '2014-08-20%'
-id:209943, category_type: 19, status: 2, task_name:亚马逊返点站CPS
-shzknn@sina.com: update task_history08 set status = 3 where id = 209943
+id:, category_type: , status: , task_name:
+shzknn@sina.com: update task_history08 set status = 3 where id = 
 shzknn@sina.com: update user set points = points + 52 where id = 1141938
-shzknn@sina.com: insert into point_history08 (user_id,point_change_num,reason,create_time) values (1141938,52,19,'2014-12-03 16:39:31')
+shzknn@sina.com: insert into point_history08 (user_id,point_change_num,reason,create_time) values (1141938,52,,'2014-12-05 17:52:56')
 
 
 386022827@qq.com: SELECT id,email,points FROM user WHERE email = '386022827@qq.com'
-user_id:1132798, email: 386022827@qq.com, points: 603
+email: 386022827@qq.com, user_id:1132798, points: 603
 386022827@qq.com: select * from task_history08 where user_id =1132798 and point = 15 and task_name like '%亚马逊%' and date like '2014-07-08%'
-id:166877, category_type: 19, status: 2, task_name:亚马逊返点站CPS
-386022827@qq.com: update task_history08 set status = 3 where id = 166877
+id:, category_type: , status: , task_name:
+386022827@qq.com: update task_history08 set status = 3 where id = 
 386022827@qq.com: update user set points = points + 15 where id = 1132798
-386022827@qq.com: insert into point_history08 (user_id,point_change_num,reason,create_time) values (1132798,15,19,'2014-12-03 16:39:31')
+386022827@qq.com: insert into point_history08 (user_id,point_change_num,reason,create_time) values (1132798,15,,'2014-12-05 17:52:57')
 
 
-完成了
+Success!
+
