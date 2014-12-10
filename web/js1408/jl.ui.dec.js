dd52a5e8f68cc5b25be1a5e65a58872a845e2e59
wltonline <wlt_liting@163.com>
2014-12-10 13:56:53 +0800

    #537

--- web/js1408/jl.ui.dec.js
+++ web/js1408/jl.ui.dec.js
@@ -82,14 +82,18 @@ function runFlow(){
 }
 function checkFlow(){
     var decRule = $('.decRule'), decRuleBtn = $('.decRuleBtn, .decTopCon .goBtn, .noStart, .timestamp');
-    decRuleBtn.on('click', function(){
-        var datepicker = $( "#datepicker, .defaultTxt" );
+	decRuleBtn.on('click', function(){
+		var datepicker = $( "#datepicker, .defaultTxt" );
         datepicker.datetimepicker({ lang : 'ch', parentID: '.decRuleFlow', timepicker : false, format : 'Y-m-d', formatDate : 'Y-m-d' });
-        decRule.animate({ right: '0px'}, 500);
-    });
-    decRule.on('mouseleave', function(){
-        $(this).animate({ right: '-405px'}, 500);
-    });
+		if($(this).hasClass('decRuleBtnActive')){
+			$('.decRuleBtn').removeClass('decRuleBtnActive');
+			decRule.animate({ right: '-405px'}, 500);
+		}else{
+			decRule.animate({ right: '0px'}, 500, function(){
+				$('.decRuleBtn').addClass('decRuleBtnActive');
+			});
+		}
+	});
     formClear();
 }
 function countDown(){
@@ -140,13 +144,14 @@ function countDown(){
 function topFold(){
     $('.timestamp').hide();
     setTimeout(function(){
+		$('.goBtn').fadeOut('fast');
+		$('.decTopConBg').animate({ height: '119px', overflow: 'hidden'}, 1E3);
         $('.decTop').addClass('decTopFold').animate({ height: '119px'}, 1E3, function(){
             $('.timestamp').fadeIn();
-            $('.goBtn').hide();
             $('.decTopConBg').fadeOut();
             runFlow();
         });
-    }, 2E3);
+    }, 5E3);
 }
 function addMask(parentEle){
     var wrapper = '<div></div>';
@@ -178,7 +183,7 @@ $(function(){
         beginAjax: function(){
             var $this = this;
             var opts = $this.options;
-            $.ajax({
+            /*$.ajax({
                 url: Routing.generate('jili_frontend_decemberactivity_geteggsinfo'),
                 type: 'post',
                 dataType: 'json',
@@ -193,7 +198,13 @@ $(function(){
                 error: function(){
                     $this.debug('第一次请求失败……');
                 }
-            });
+            });*/
+			var eggData = { code: '1', msg: '', data: { token: '', validNum: 3, comfortNum: 2, diffMoney: 200, isStart: false}};
+					$this.debug('初始金蛋信息……', eggData);
+                    $this.showEgg(eggData);
+                    $this.setEggInfo(eggData);
+                    $this.addEgg(eggData);
+                    $this.openStart(eggData);
         },
         init: function(){
             this.beginAjax();
@@ -221,17 +232,23 @@ $(function(){
         addEgg: function(initData){
             var $this = this;
             var opts = $this.options;
-            var eggWrapper = '<li><div><img src="/images/december/egg.gif" width="110" height="138"/></div><span>我要砸蛋</span></li>';
+			var randNum;
+			var imgArr = ["/images/december/static_egg.gif", "/images/december/shaking_egg7.gif", "/images/december/shaking_egg12.gif"];
+            var eggWrapper = '<li><div><img src="#" width="110" height="138"/></div><span>我要砸蛋</span></li>';
             for(var i = 0; i< initData.data.validNum; i++){
-                $(opts.container).append(eggWrapper);
+				randNum = Math.floor(Math.random()*(0-3) + 3);
+				$this.debug(randNum);
+				$(eggWrapper).find('img').attr("src", imgArr[randNum]).end().appendTo($(opts.container));
             }
             for(var j = 0; j< initData.data.comfortNum; j++){
-                $(opts.container).append($(eggWrapper).addClass('comfort'));
+				randNum = Math.floor(Math.random()*(0-3) + 3);
+				$this.debug(randNum);
+				$(eggWrapper).find('img').attr("src", imgArr[randNum]).end().addClass('comfort').appendTo($(opts.container));
             }
         },
         getResult: function(initData, eggType){
             var $this = this;
-            $.ajax({
+            /*$.ajax({
                 url: Routing.generate('jili_frontend_decemberactivity_breakegg'),
                 type: 'post',
                 dataType: 'json',
@@ -244,7 +261,10 @@ $(function(){
                 error: function(){
                     $this.debug('第二次请求结果失败……');
                 }
-            });
+            });*/
+			var resultData = { code: 1, msg: '', data: { token: '', points: 50}};
+					$this.debug('砸蛋结果……', resultData);
+                    $this.showResult(resultData);
         },
         showResult: function(resultData){
             this.debug('展示砸蛋结果……');
