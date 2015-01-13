c3b817236aa6cc380fafa2365bbe974239332a57
Jarod Chiang <chiangtor@gmail.com>
2015-01-13 10:49:01 +0800

    #618

--- src/Jili/ApiBundle/Resources/doc/issue618.sql
+++ src/Jili/ApiBundle/Resources/doc/issue618.sql
@@ -1,21 +1,11 @@
--- 点击表
-CREATE TABLE `activity_gathering_checkin_log` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `checkin_at` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_id` (`user_id`),
-  CONSTRAINT `activity_gathering_checkin_log_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 订单表
 CREATE TABLE `activity_gathering_taobao_order` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_identity` varchar(255) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `created_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uniq_order_user` (`order_identity`,`user_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `activity_gathering_taobao_order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `order_identity` varchar(255) NOT NULL,
+      `user_id` int(11) NOT NULL,
+      `created_at` datetime DEFAULT NULL,
+      PRIMARY KEY (`id`),
+      UNIQUE KEY `user_id` (`user_id`),
+      CONSTRAINT `activity_gathering_taobao_order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
