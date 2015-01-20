3e40561bc3840caf106e6eaec5fef7ee8ba5f958
Jarod Chiang <chiangtor@gmail.com>
2015-01-20 10:44:24 +0800

    eggs breaker fixed

--- src/Jili/FrontendBundle/Controller/DecemberActivityController.php
+++ src/Jili/FrontendBundle/Controller/DecemberActivityController.php
@@ -6,11 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
-
 use Jili\FrontendBundle\Form\Type\GameEggsBreakerTaoBaoOrderType;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Response;
 use Jili\BackendBundle\Utility\TaobaoOrderToEggs;
 use Jili\FrontendBundle\Entity\GameEggsBreakerTaobaoOrder;
@@ -65,7 +62,7 @@ class DecemberActivityController extends Controller
         $now = new \Datetime();
 
         if($now > $startAt ) {
-            return new Reponse('');
+            return new Response('');
         }
 
         $request = $this->get('request');
