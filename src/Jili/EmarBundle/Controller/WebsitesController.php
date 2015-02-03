4de0136a4ee93b0456673aa57cb769f4ee2d2c76
Jarod Chiang <chiangtor@gmail.com>
2015-02-03 13:09:04 +0800

    #652

--- src/Jili/EmarBundle/Controller/WebsitesController.php
+++ src/Jili/EmarBundle/Controller/WebsitesController.php
@@ -286,7 +286,6 @@ class WebsitesController extends Controller
         $request = $this->get('request');
         $logger = $this->get('logger');
         
-        var_dump($this->container->getParameter('emar_com.api.websites.list_get.depreacted_web_ids'));
         if( in_array($wid,
             $this->container->getParameter('emar_com.api.websites.list_get.depreacted_web_ids'))) {
                 throw $this->createNotFoundException( '没找到此商家.');
