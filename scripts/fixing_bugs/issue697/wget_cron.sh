cfcdb3436d035ff2cccd597a4adde7138756176e
Jarod Chiang <chiangtor@gmail.com>
2015-04-20 16:41:45 +0800

    #697

--- scripts/fixing_bugs/issue697/wget_cron.sh
+++ scripts/fixing_bugs/issue697/wget_cron.sh
@@ -7,50 +7,15 @@ DT=$(date +"%Y%m%d%H%M%S")
 # 取apache 限制的1/2 到1/3即可。
 max_request_per_second=100000
 
-
-# 找到3/15/2015 ~ 4/14/2015 期间,
-perl -Ilib/ accesslog_parser.pl > ~/access_log_${DT}.log
-
-# accesslog  
-grep 'query_string' ~/access_log_${DT}.log | sed -e 's/^.*query_string: //' > ~/access_log_${DT}_x.log
-
-sed -e '/APIMemberId/d' ~/access_log_${DT}_x.log
-
 i=0; 
+j=0;
 t1=$(date +%s)
-for x in $(cat ~/access_log_${DT}_x.log)
-do
-    if [[ $i -ge $max_request_per_second ]]; then
-        #statements
-        t2=$(date +%s)
-        let "diff=t2-t1"
-        echo 'processed ', $diff , 's';
-        if [[ $diff -lt  1  ]]  ; then
-            let "to_sleep=1-diff"
-            echo 'wait ', $to_sleep , 's';
-            sleep $to_sleep
-        fi
-        let "i=0";
-        let "t1=$(date +%s)"
-    fi
-
-#  235上测试
-echo     wget -P/tmp/g -a /tmp/wget.g.log  "http://jarod-jili.com/app_dev.php$x"
-done
-
-exit;
 
 
 # 找到2/27/2015 ~ 3/14/2015 期间,
 perl -Ilib/ csv_parser.pl > ~/confirmed_${DT}.log
 grep 'WGET|||' ~/confirmed_${DT}.log| sed -e "s/\ *WGET|||//g" > ~/confirmed_${DT}_x.log
-
-
 # confirmed data 
-
-let "i=0"; 
-let "t1=$(date +%s)"
-
 for x in $(cat ~/confirmed_${DT}_x.log)
 do
     if [[ $i -ge $max_request_per_second ]]; then
@@ -68,8 +33,42 @@ do
         let "t1=$(date +%s)"
     fi
 
-#  235上测试
-echo     wget -P/tmp/h  -a /tmp/wget.h.log  "http://jarod-jili.com/app_dev.php/emar/api/callback?$x"
+    #  235上测试
+    #wget -O $j -P/tmp/h  -a /tmp/wget.h.log  "http://jarod-jili.com/app_dev.php/emar/api/callback?$x"
+    wget -O /tmp/yqfconfirmed_output.$j -P/tmp/yqfconfirmed/wgetlog/ -a /tmp/wget.yqfconfirmed.log  "http://jarod-jili.com/app_dev.php$x"
+    let "j+=1"; 
 done
 
 
+let "i=0"; 
+let "j=0"; 
+let "t1=$(date +%s)"
+
+
+
+# 找到3/15/2015 ~ 4/14/2015 期间,
+perl -Ilib/ accesslog_parser.pl > ~/access_log_${DT}.log
+# accesslog  
+grep 'query_string' ~/access_log_${DT}.log | sed -e 's/^.*query_string: //' > ~/access_log_${DT}_x.log
+sed -e '/APIMemberId/d' ~/access_log_${DT}_x.log
+
+for x in $(cat ~/access_log_${DT}_x.log)
+do
+    if [[ $i -ge $max_request_per_second ]]; then
+        #statements
+        t2=$(date +%s)
+        let "diff=t2-t1"
+        echo 'processed ', $diff , 's';
+        if [[ $diff -lt  1  ]]  ; then
+            let "to_sleep=1-diff"
+            echo 'wait ', $to_sleep , 's';
+            sleep $to_sleep
+        fi
+        let "i=0";
+        let "t1=$(date +%s)"
+    fi
+
+    #  235上测试
+    wget -O /tmp/httpaccess_output.$j -P/tmp/httpaccess/wgetlog/ -a /tmp/wget.httpaccess.log  "http://jarod-jili.com/app_dev.php/emar/api/callback?$x"
+    let "j+=1";
+done
