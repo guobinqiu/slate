7ae7eac76b2591c84e9220e42cf7fed1f6299696
Jarod Chiang <chiangtor@gmail.com>
2015-04-20 16:56:28 +0800

    Update wget_cron.sh

--- scripts/fixing_bugs/issue697/wget_cron.sh
+++ scripts/fixing_bugs/issue697/wget_cron.sh
@@ -15,7 +15,7 @@ t1=$(date +%s)
 # 找到2/27/2015 ~ 3/14/2015 期间,
 perl -Ilib/ csv_parser.pl > ~/confirmed_${DT}.log
 grep 'WGET|||' ~/confirmed_${DT}.log| sed -e "s/\ *WGET|||//g" > ~/confirmed_${DT}_x.log
-mkdir  -p /tmp/yqfconfirmed_output/
+mkdir  -p /tmp/yqfconfirmed_output/ /tmp/yqfconfirmed_wgetlog/
 # confirmed data 
 for x in $(cat ~/confirmed_${DT}_x.log)
 do
@@ -36,7 +36,7 @@ do
 
     #  235上测试
     #wget -O $j -P/tmp/h  -a /tmp/wget.h.log  "http://jarod-jili.com/app_dev.php/emar/api/callback?$x"
-    wget -O /tmp/yqfconfirmed_output/$j -P/tmp/yqfconfirmed/wgetlog/ -a /tmp/wget.yqfconfirmed.log  "http://jarod-jili.com/app_dev.php$x"
+    wget -O /tmp/yqfconfirmed_output/$j -P/tmp/yqfconfirmed_wgetlog/ -a /tmp/wget.yqfconfirmed.log  "http://jarod-jili.com/app_dev.php$x"
     let "j+=1"; 
 done
 
@@ -52,7 +52,7 @@ perl -Ilib/ accesslog_parser.pl > ~/access_log_${DT}.log
 # accesslog  
 grep 'query_string' ~/access_log_${DT}.log | sed -e 's/^.*query_string: //' > ~/access_log_${DT}_x.log
 sed -i'' -e '/APIMemberId/d' ~/access_log_${DT}_x.log
-mkdir -p /tmp/httpaccess_output/
+mkdir -p /tmp/httpaccess_output/ /tmp/httpaccess_wgetlog/
 for x in $(cat ~/access_log_${DT}_x.log)
 do
     if [[ $i -ge $max_request_per_second ]]; then
@@ -70,6 +70,6 @@ do
     fi
 
     #  235上测试
-    wget -O /tmp/httpaccess_output/$j -P/tmp/httpaccess/wgetlog/ -a /tmp/wget.httpaccess.log  "http://jarod-jili.com/app_dev.php/emar/api/callback?$x"
+    wget -O /tmp/httpaccess_output/$j -P/tmp/httpaccess_wgetlog/ -a /tmp/wget.httpaccess.log  "http://jarod-jili.com/app_dev.php/emar/api/callback?$x"
     let "j+=1";
 done
