c4a4e28cb8b8504ef8f0fd2bb112715ea3932489
Jarod Chiang <chiangtor@gmail.com>
2015-04-24 06:49:03 +0800

    #680

--- src/Jili/FrontendBundle/Entity/DuomaiCommissionData.php
+++ src/Jili/FrontendBundle/Entity/DuomaiCommissionData.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="duomai_commission_data")
  * @ORM\Entity
  */
-class DuomaiCommissionData
+class DuomaiCommissionData extends CommissionDataBase
 {
     /**
      * @var integer
