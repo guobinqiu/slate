df7ceb9e9a743ffa959b6ba01e922a9987920ba7
zhangmm <zhangmm@voyagegroup.com.cn>
2015-08-26 13:27:32 +0800

    migration region data

---
+++ migration/doc/region.sql
@@ -0,0 +1,50 @@
+/*
+CREATE TABLE `panel_region` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `panel_country_id` int(11) NOT NULL,
+   `name` varchar(255) DEFAULT NULL,
+   `name_native` varchar(255) DEFAULT NULL,
+   `name_en` varchar(255) DEFAULT NULL,
+   `category_name` varchar(255) DEFAULT NULL,
+   `category_name_en` varchar(255) DEFAULT NULL,
+   `sort_key` int(11) DEFAULT NULL,
+   `visible_flag` tinyint(4) DEFAULT NULL,
+   `delete_flag` tinyint(4) DEFAULT NULL,
+   `updated_at` datetime DEFAULT NULL,
+   `created_at` datetime DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   KEY `FI__21` (`panel_country_id`),
+   CONSTRAINT `Rel_21` FOREIGN KEY (`panel_country_id`) REFERENCES `panel_country` (`id`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=3800 DEFAULT CHARSET=utf8
+
+
+CREATE TABLE `provinceList` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `provinceName` varchar(50) NOT NULL,
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8
+
+CREATE TABLE `cityList` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `cityName` varchar(50) NOT NULL,
+   `provinceId` int(11) NOT NULL,
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=352 DEFAULT CHARSET=utf8
+*/
+
+CREATE TABLE IF NOT EXISTS `migration_region_mapping` (
+  `region_id` int(11) NOT NULL,
+  `province_id` int(11) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  UNIQUE KEY `region_id` (`region_id`,`province_id`,`city_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--todo: 是否要加上这三个字段：sort_key, visible_flag, delete_flag 
+--根据数据显示，可以不要这三个字段
+
+UPDATE cityList SET cityName = '恩施土家族苗族自治州' WHERE cityName = '恩施土家族苗族自治区';
+UPDATE cityList SET cityName = '湘西土家族苗族自治州' WHERE cityName = '湘西土家族苗族自治区';
+UPDATE cityList SET cityName = '延边朝鲜族自治州' WHERE cityName = '延边朝鲜族自治区';
+UPDATE cityList SET cityName = '兴安盟' WHERE cityName = '兴安盟市';
+UPDATE cityList SET cityName = '楚雄彝族自治州' WHERE cityName = '楚雄彝族自治区';
+UPDATE cityList SET cityName = '怒江傈僳族自治州' WHERE cityName = '怒江傈僳族自治区';
---
+++ migration/script/region.php
@@ -0,0 +1,114 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+
+$log = "log_" . date('Ymd') . ".sql";
+$log_handle = fopen($log, "w");
+
+$file_handle = fopen('panel_region.csv', "r");
+
+$contents = null;
+if ($file_handle !== FALSE) {
+    while (($data = fgetcsv($file_handle, 1000, ",")) !== FALSE) {
+        $contents[] = $data;
+    }
+}
+fclose($file_handle);
+unset($contents[0]);
+
+//根据命令行参数，决定是否正式执行
+$definitive = false;
+if (isset($argv[1])) {
+    $definitive = $argv[1];
+}
+
+try {
+    $dsn = "mysql:host=localhost;dbname=jili_db_new";
+    $dbh = new PDO($dsn, 'root', 'ecnavi');
+    $dbh->exec("SET NAMES 'utf8';");
+
+    $dbh->beginTransaction();
+
+    foreach ($contents as $value) {
+
+        $region_id = trim($value[0]);
+        $provinceName = trim($value[5]);
+        $cityName = trim($value[2]);
+
+        $municipality = array (
+            '上海市',
+            '北京市',
+            '天津市',
+            '重庆市'
+        );
+        if (in_array($provinceName, $municipality)) {
+            $cityName = $provinceName;
+            $provinceName = '直辖市';
+        }
+
+        if ($provinceName == '甘肃' && $cityName == '兰州') {
+            $cityName = '兰州市';
+        }
+
+        if ($provinceName == '珠海' && $cityName == '广东省') {
+            $cityName = '珠海市';
+        }
+
+        if ($provinceName == '襄樊市' && $cityName == '湖北省') {
+            $cityName = '襄阳市';
+        }
+
+        if ($provinceName == '新疆维吾尔自治区' && $cityName == '省直辖行政单位') {
+            $cityName = '其他省直辖行政单位';
+        }
+
+        $sql = "SELECT id FROM provinceList WHERE provinceName = '" . $provinceName . "'";
+        fwrite($log_handle, $value[0] . ": " . $sql . "\r\n");
+        $sth = $dbh->prepare($sql);
+        $sth->execute();
+        $provinceId = $sth->fetchColumn();
+
+        if (!$provinceId) {
+            $p_sql = "insert into provinceList(provinceName) values('$provinceName')";
+            fwrite($log_handle, $value[0] . ": " . $p_sql . "\r\n");
+            $sth = $dbh->prepare($p_sql);
+            $sth->execute();
+            $provinceId = $dbh->lastInsertId();
+        }
+
+        $sql = "SELECT id FROM cityList WHERE provinceId = $provinceId and cityName = '" . $cityName . "'";
+        fwrite($log_handle, $value[0] . ": " . $sql . "\r\n");
+        $sth = $dbh->prepare($sql);
+        $sth->execute();
+        $cityId = $sth->fetchColumn();
+
+        if (!$cityId) {
+            $c_sql = "insert into cityList(cityName,provinceId) values('$cityName', $provinceId)";
+            fwrite($log_handle, $value[0] . ": " . $c_sql . "\r\n");
+            $sth = $dbh->prepare($c_sql);
+            $sth->execute();
+            $cityId = $dbh->lastInsertId();
+        }
+
+        $sql = "insert into migration_region_mapping values($region_id, $provinceId, $cityId)";
+        fwrite($log_handle, $value[0] . ": " . $sql . "\r\n");
+        $sth = $dbh->prepare($sql);
+        $sth->execute();
+    }
+
+    if ($definitive) {
+        $dbh->commit();
+        echo "commit" . "\r\n";
+    } else {
+        echo "not commit" . "\r\n";
+    }
+} catch (PDOException $ex) {
+    $dbh->rollBack();
+    echo "rollBack" . "\r\n";
+    echo $ex->getMessage();
+}
+
+fwrite($log_handle, "end" . "\r\n");
+fclose($log_handle);
+echo "Done!";
+exit();
+?>
