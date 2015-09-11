14b7a60cf945288794f9b3b0015f40d66c526de5
Jarod Chiang <chiangtor@gmail.com>
2015-09-11 14:16:28 +0800

    updated result code to v2.1

--- src/Jili/ApiBundle/Tests/Services/Flow/FlowOrderCreateApiProcessorTest.php
+++ src/Jili/ApiBundle/Tests/Services/Flow/FlowOrderCreateApiProcessorTest.php
@@ -17,7 +17,7 @@ class FlowOrderCreateApiProcessorTest extends KernelTestCase {
         // get service
         $service = $container->get('flow_ordercreate.processor');
 
-        $param['custom_product_id'] = '30010';
+        $param['custom_product_id'] = '30500';
         $param['mobile'] = '18016018626';
         $param['custom_order_sn'] = 1;
         $return = $service->process($param);
@@ -26,7 +26,7 @@ class FlowOrderCreateApiProcessorTest extends KernelTestCase {
         $this->assertNotNull($return['custom_order_sn']);
 
         //错误场合
-        $param['custom_product_id'] = '300100';
+        $param['custom_product_id'] = '30500';
         $param['mobile'] = '18016018626';
         $param['custom_order_sn'] = 1;
         $return = $service->process($param);
@@ -34,11 +34,12 @@ class FlowOrderCreateApiProcessorTest extends KernelTestCase {
         $this->assertEquals('对不起，不能兑换，请联系客服！', $return['error_message']);
 
         //错误场合 手机号码不正确
-        $param['custom_product_id'] = '300100';
+        $param['custom_product_id'] = '30100';
         $param['mobile'] = '1801601862600';
         $param['custom_order_sn'] = 10000;
         $return = $service->process($param);
-        $this->assertEquals(205, $return['resultcode']);
-        $this->assertEquals(' 请求的产品不正确', $return['error_message']);
+        $this->assertEquals(206, $return['resultcode']);
+        $this->assertEquals('手机号码不正确', $return['error_message']);
+
     }
 }
