216fe33f76eb62aa47e64b4d8d66ca9afeec735c
wltonline <wlt_liting@163.com>
2015-09-30 14:51:22 +0800

    新建模板文件 兑换中心微调

--- src/Wenwen/FrontendBundle/Controller/TestController.php
+++ src/Wenwen/FrontendBundle/Controller/TestController.php
@@ -24,4 +24,22 @@ class TestController extends Controller {
     {
         return $this->render('WenwenFrontendBundle:Test:index.html.twig');
     }
+
+    /**
+     * @Route("/indexSimple")
+     * @Template
+     */
+    public function indexSimpleAction()
+    {
+        return $this->render('WenwenFrontendBundle:Test:indexSimple.html.twig');
+    }
+
+    /**
+     * @Route("/indexSimple2")
+     * @Template
+     */
+    public function indexSurveyAction()
+    {
+        return $this->render('WenwenFrontendBundle:Test:indexSimple2.html.twig');
+    }
 }
