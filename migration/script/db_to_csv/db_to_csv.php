38889943a4c08cebf3c112df5f1051ae9e93c829
Jarod <tao.jiang@d8aspring.com>
2015-12-02 16:21:38 +0800

    added page when dump db to csv

--- migration/script/db_to_csv/db_to_csv.php
+++ migration/script/db_to_csv/db_to_csv.php
@@ -4,10 +4,10 @@ $fh = fopen('php://stdout','w');
 
 
 if($argc != 7 ) {
-  file_put_contents('php://stderr','bad args');
+  file_put_contents('php://stderr','bad args:'. var_export($argv, true));
   exit;
-  
 }
+
 $server = $argv[1];
 $user = $argv[2];
 $password = $argv[3];
@@ -22,12 +22,13 @@ $options = array(
 );
 try {
   $dbh = new PDO($dsn, $user, $password,$options);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   file_put_contents('php://stderr','Connection failed: ' . $e->getMessage());
   fclose($fh);
   exit;
 }
-
+# get csv head line
 $q = $dbh->prepare("DESCRIBE $table");
 $q->execute();
 $table_fields = $q->fetchAll(PDO::FETCH_COLUMN);
@@ -35,10 +36,54 @@ fputcsv( $fh, $table_fields);
 
 $q->closeCursor();
 
-foreach ($dbh->query($sql) as $row) {
-  fputcsv( $fh, $row );
-}
+ # pagenation
+ $q = $dbh->prepare("select count(*) from $table" );
+ $q->execute();
+ $row = $q->fetch(PDO::FETCH_NUM);
+ $total = $row[0];
+ $page_size= 100000;
+ 
+ $pages =  ceil($total/$page_size );
+ $start = 0;
+ for( $page_no = 0; $page_no < $pages ; $page_no++ ) {
+
+  $query  =  $sql . " limit $start, $page_size";
+#  echo $query,PHP_EOL;
+  $result = $dbh->query($query);
+  $result->setFetchMode(PDO::FETCH_NUM);
+  while($row = $result->fetch()) { 
+    fputcsv( $fh, $row );
+  }
+
+  $result->closeCursor();
+
+  $start =  $start + $page_size ; 
+ }
+ 
+
+
+
+#$result = $dbh->query($sql);
+#$result->setFetchMode(PDO::FETCH_NUM);
+#while($row = $result->fetch()) { 
+#  fputcsv( $fh, $row );
+#}
+#if(empty($sql)) {
+#  file_put_contents('php://stderr','bad args:'. var_export($argv, true));
+#  exit;
+#}
+#$line = 0;
+#$stmt=$dbh->prepare($sql);
+#$stmt->execute();
+#do {
+#  $row =  $stmt->fetch(); 
+#  fputcsv( $fh, $row );
+#  $stmt->closeCursor(); 
+#  ++$line; 
+#} while($stmt->nextRowset());
 
 
 fclose($fh);
+
+# echo 'line:', $line, PHP_EOL;
 
