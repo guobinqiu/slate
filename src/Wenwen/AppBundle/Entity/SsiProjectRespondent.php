32774c4024e9a8c04f3d3d08c3c51d185c798dd8
Jarod Chiang <chiangtor@gmail.com>
2016-01-29 11:27:23 +0800

    generated ssi related entities

---
+++ src/Wenwen/AppBundle/Entity/SsiProjectRespondent.php
@@ -0,0 +1,311 @@
+<?php
+
+namespace Wenwen\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * SsiProjectRespondent
+ *
+ * @ORM\Table(name="ssi_project_respondent", uniqueConstraints={@ORM\UniqueConstraint(name="ssi_respondent_uniq", columns={"ssi_project_id", "ssi_respondent_id"})}, indexes={@ORM\Index(name="ssi_project_mail_batch_idx", columns={"ssi_project_id", "ssi_mail_batch_id"}), @ORM\Index(name="ssi_respondent_idx", columns={"ssi_respondent_id"}), @ORM\Index(name="updated_at_answer_status_idx", columns={"updated_at", "answer_status"}), @ORM\Index(name="IDX_DCEFA6E9EBD1F782", columns={"ssi_project_id"})})
+ * @ORM\Entity
+ */
+class SsiProjectRespondent
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="ssi_mail_batch_id", type="integer", nullable=false)
+     */
+    private $ssiMailBatchId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="start_url_id", type="string", length=255, nullable=false)
+     */
+    private $startUrlId;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="answer_status", type="boolean", nullable=false)
+     */
+    private $answerStatus;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="stash_data", type="text", nullable=true)
+     */
+    private $stashData;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="completed_at", type="datetime", nullable=true)
+     */
+    private $completedAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=false)
+     */
+    private $updatedAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * @var \Wenwen\AppBundle\Entity\SsiProject
+     *
+     * @ORM\ManyToOne(targetEntity="Wenwen\AppBundle\Entity\SsiProject")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="ssi_project_id", referencedColumnName="id")
+     * })
+     */
+    private $ssiProject;
+
+    /**
+     * @var \Wenwen\AppBundle\Entity\SsiRespondent
+     *
+     * @ORM\ManyToOne(targetEntity="Wenwen\AppBundle\Entity\SsiRespondent")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="ssi_respondent_id", referencedColumnName="id")
+     * })
+     */
+    private $ssiRespondent;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set ssiMailBatchId
+     *
+     * @param integer $ssiMailBatchId
+     * @return SsiProjectRespondent
+     */
+    public function setSsiMailBatchId($ssiMailBatchId)
+    {
+        $this->ssiMailBatchId = $ssiMailBatchId;
+
+        return $this;
+    }
+
+    /**
+     * Get ssiMailBatchId
+     *
+     * @return integer 
+     */
+    public function getSsiMailBatchId()
+    {
+        return $this->ssiMailBatchId;
+    }
+
+    /**
+     * Set startUrlId
+     *
+     * @param string $startUrlId
+     * @return SsiProjectRespondent
+     */
+    public function setStartUrlId($startUrlId)
+    {
+        $this->startUrlId = $startUrlId;
+
+        return $this;
+    }
+
+    /**
+     * Get startUrlId
+     *
+     * @return string 
+     */
+    public function getStartUrlId()
+    {
+        return $this->startUrlId;
+    }
+
+    /**
+     * Set answerStatus
+     *
+     * @param boolean $answerStatus
+     * @return SsiProjectRespondent
+     */
+    public function setAnswerStatus($answerStatus)
+    {
+        $this->answerStatus = $answerStatus;
+
+        return $this;
+    }
+
+    /**
+     * Get answerStatus
+     *
+     * @return boolean 
+     */
+    public function getAnswerStatus()
+    {
+        return $this->answerStatus;
+    }
+
+    /**
+     * Set stashData
+     *
+     * @param string $stashData
+     * @return SsiProjectRespondent
+     */
+    public function setStashData($stashData)
+    {
+        $this->stashData = $stashData;
+
+        return $this;
+    }
+
+    /**
+     * Get stashData
+     *
+     * @return string 
+     */
+    public function getStashData()
+    {
+        return $this->stashData;
+    }
+
+    /**
+     * Set completedAt
+     *
+     * @param \DateTime $completedAt
+     * @return SsiProjectRespondent
+     */
+    public function setCompletedAt($completedAt)
+    {
+        $this->completedAt = $completedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get completedAt
+     *
+     * @return \DateTime 
+     */
+    public function getCompletedAt()
+    {
+        return $this->completedAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return SsiProjectRespondent
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return SsiProjectRespondent
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set ssiProject
+     *
+     * @param \Wenwen\AppBundle\Entity\SsiProject $ssiProject
+     * @return SsiProjectRespondent
+     */
+    public function setSsiProject(\Wenwen\AppBundle\Entity\SsiProject $ssiProject = null)
+    {
+        $this->ssiProject = $ssiProject;
+
+        return $this;
+    }
+
+    /**
+     * Get ssiProject
+     *
+     * @return \Wenwen\AppBundle\Entity\SsiProject 
+     */
+    public function getSsiProject()
+    {
+        return $this->ssiProject;
+    }
+
+    /**
+     * Set ssiRespondent
+     *
+     * @param \Wenwen\AppBundle\Entity\SsiRespondent $ssiRespondent
+     * @return SsiProjectRespondent
+     */
+    public function setSsiRespondent(\Wenwen\AppBundle\Entity\SsiRespondent $ssiRespondent = null)
+    {
+        $this->ssiRespondent = $ssiRespondent;
+
+        return $this;
+    }
+
+    /**
+     * Get ssiRespondent
+     *
+     * @return \Wenwen\AppBundle\Entity\SsiRespondent 
+     */
+    public function getSsiRespondent()
+    {
+        return $this->ssiRespondent;
+    }
+}
