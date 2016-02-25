4b95d11af1b3dadd5efc762a86256feabd4c35ff
zhangmm <zhangmm@voyagegroup.com.cn>
2016-02-25 17:27:40 +0800

    add shell script

---
+++ bin/schedule/reward-cint-point.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# add crontab sample
+# 0 2 * * * /bin/sh /var/www/html/jili/bin/schedule/reward-cint-point.sh &2>/data/91jili/logs/cron/reward-cint-point.log &1>/dev/null 
+
+cd $(dirname $0)/../../;
+./app/console panel:reward-cint-point --definitive `date+%Y-%m-%d --date="-1 day"` -e prod
