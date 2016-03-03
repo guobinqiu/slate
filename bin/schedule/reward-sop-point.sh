05ec89d24cbfb111648fbc6df58ca01dd0950522
zhangmm <zhangmm@voyagegroup.com.cn>
2016-03-03 15:08:33 +0800

    modify bash script

--- bin/schedule/reward-sop-point.sh
+++ bin/schedule/reward-sop-point.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 # add crontab sample
-# 0 2 * * * /bin/sh /var/www/html/jili/bin/schedule/reward-sop-point.sh &2>/data/91jili/logs/cron/reward-sop-point.log &1>/dev/null 
+# 0 2 * * * /bin/sh /var/www/html/jili/bin/schedule/reward-sop-point.sh &2>/data/91jili/logs/cron/reward-sop-point.log &1>/dev/null
 
 cd $(dirname $0)/../../;
-./app/console panel:reward-sop-point --definitive date +%Y-%m-%d --date="-1 day" -e prod
+./app/console panel:reward-sop-point --definitive `date +%Y-%m-%d --date="-1 day"` -e prod
+
