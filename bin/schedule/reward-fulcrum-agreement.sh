b0748cf05127db045f00659ac0ef67438cf5856a
Jarod Chiang <chiangtor@gmail.com>
2016-03-04 12:01:23 +0800

    Update reward-fulcrum-agreement.sh

--- bin/schedule/reward-fulcrum-agreement.sh
+++ bin/schedule/reward-fulcrum-agreement.sh
@@ -3,5 +3,5 @@
 # 0 2 * * * /bin/sh /var/www/html/jili/bin/schedule/reward-fulcrum-agreement.sh 2>/data/91jili/logs/cron/reward-fulcrum-agreement.log 1>/dev/null 
 
 cd $(dirname $0)/../../;
-./app/console panel:reward-fulcrum-agreement --definitive `date +%Y-%m-%d -d="-1 day"` -e prod
+./app/console panel:reward-fulcrum-agreement --definitive `date +%Y-%m-%d -d "-1 day"` -e prod
 
