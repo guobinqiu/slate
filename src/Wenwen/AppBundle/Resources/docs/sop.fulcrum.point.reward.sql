32c0401e5643fc0447bcc37a7723837d73428a17
Jarod Chiang <chiangtor@gmail.com>
2016-03-04 16:15:32 +0800

    Update sop.fulcrum.point.reward.sql

--- src/Wenwen/AppBundle/Resources/docs/sop.fulcrum.point.reward.sql
+++ src/Wenwen/AppBundle/Resources/docs/sop.fulcrum.point.reward.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `fulcrum_research_survey_participation_history` (
   `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `fulcrum_project_member_uniq` (`fulcrum_project_id`,`app_member_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `fulcrum_user_agreement_participation_history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -21,5 +21,5 @@ CREATE TABLE IF NOT EXISTS `fulcrum_user_agreement_participation_history` (
   `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `app_member_id_uniq_key` (`app_member_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
 
