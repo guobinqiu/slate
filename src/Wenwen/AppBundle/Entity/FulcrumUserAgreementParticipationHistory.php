4e6466604e3f45e5a8fe53bcf51a0c0c57f768cd
Jarod Chiang <chiangtor@gmail.com>
2016-03-04 16:08:34 +0800

    Update FulcrumUserAgreementParticipationHistory.php

--- src/Wenwen/AppBundle/Entity/FulcrumUserAgreementParticipationHistory.php
+++ src/Wenwen/AppBundle/Entity/FulcrumUserAgreementParticipationHistory.php
@@ -17,7 +17,7 @@ class FulcrumUserAgreementParticipationHistory
      *
      * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY"))
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $id;
 
