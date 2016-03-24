eb24e6a2d258021efd6745763ec3593474bb99a9
zhangmm <zhangmm@voyagegroup.com.cn>
2016-03-24 16:18:52 +0800

    fix load data for testunit

--- src/Jili/ApiBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Jili/ApiBundle/DataFixtures/ORM/LoadUserData.php
@@ -46,6 +46,7 @@ class LoadUserData extends AbstractFixture implements ContainerAwareInterface, F
         $user->setEmail('user@voyagegroup.com.cn');
         $user->setPoints(100);
         $user->setIsInfoSet(0);
+        $user->setIconPath('test/test_icon.jpg');
         $user->setRewardMultiple(1);
         $user->setPwd('111111');
         $user->setRegisterDate ( new \DateTime());
