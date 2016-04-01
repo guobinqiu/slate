abe1e976f454e07323b79c07b59a35914dbeefe0
miaomiao7988 <miaomiao.zhang@d8aspring.com>
2016-04-01 13:42:53 +0800

    add extraInfokeys process

--- src/Wenwen/AppBundle/Command/PanelRewardSopAdditionalPointCommand.php
+++ src/Wenwen/AppBundle/Command/PanelRewardSopAdditionalPointCommand.php
@@ -89,4 +89,9 @@ class PanelRewardSopAdditionalPointCommand extends PanelRewardCommand
         $em->persist($history_model);
         $em->flush();
     }
+
+    protected function extraInfoKeys()
+    {
+        return array('point_type');
+    }
 }
