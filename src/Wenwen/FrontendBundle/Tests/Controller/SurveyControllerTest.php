d98a474aba2cf83f75eb49551c2e307c03fff947
miaomiao7988 <miaomiao.zhang@d8aspring.com>
2016-04-06 17:57:32 +0800

    fix test code

--- src/Wenwen/FrontendBundle/Tests/Controller/SurveyControllerTest.php
+++ src/Wenwen/FrontendBundle/Tests/Controller/SurveyControllerTest.php
@@ -1,13 +1,12 @@
 <?php
-
 namespace Wenwen\FrontendBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
 use Doctrine\Common\DataFixtures\Loader;
 use Jili\ApiBundle\DataFixtures\ORM\LoadUserData;
+use Wenwen\FrontendBundle\Controller\SurveyController;
 
 class SurveyControllerTest extends WebTestCase
 {
@@ -55,6 +54,7 @@ class SurveyControllerTest extends WebTestCase
 
     /**
      * @group dev-merge-ui-survey-list
+     * @group dev-merge-ui-survey-top
      */
     public function testIndexAction()
     {
@@ -87,5 +87,62 @@ class SurveyControllerTest extends WebTestCase
         $this->assertCount(1, $crawler->filter('#sop_app_mid'));
         $this->assertCount(1, $crawler->filter('#sop_sig'));
         $this->assertCount(1, $crawler->filter('#sop_time'));
+    }
+
+    /**
+     * @group dev-merge-ui-survey-top
+     */
+    public function testTopAction()
+    {
+        $client = static::createClient();
+        $container = $client->getContainer();
+        $em = $this->em;
+
+        $url = $container->get('router')->generate('_survey_top');
+        $crawler = $client->request('GET', $url);
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $crawler = $client->followRedirect();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        //login 后
+        $session = $container->get('session');
+        $users = $em->getRepository('JiliApiBundle:User')->findAll();
+        $session->set('uid', $users[0]->getId());
+        $session->save();
+
+        $crawler = $client->request('GET', $url);
+        $this->assertEquals(301, $client->getResponse()->getStatusCode());
+        $crawler = $client->followRedirect();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertCount(1, $crawler->filter('#sop_api_url'));
+        $this->assertCount(1, $crawler->filter('#sop_point'));
+        $this->assertCount(1, $crawler->filter('#sop_app_id'));
+        $this->assertCount(1, $crawler->filter('#sop_app_mid'));
+        $this->assertCount(1, $crawler->filter('#sop_sig'));
+        $this->assertCount(1, $crawler->filter('#sop_time'));
+    }
+
+    /**
+     * @group dev-merge-ui-survey-top
+     */
+    public function testGetSopParams()
+    {
+        $client = static::createClient();
+        $container = $client->getContainer();
+        $em = $this->em;
+
+        $controller = new SurveyController();
+        $controller->setContainer($container);
+
+        $sop_respondent = $em->getRepository('JiliApiBundle:SopRespondent')->retrieveOrInsertByUserId(1);
+        $sop_config = $container->getParameter('sop');
+        $return = $controller->getSopParams($sop_config, $sop_respondent->getId());
+        $this->assertNotEmpty($return['sop_params']['app_id']);
+        $this->assertNotEmpty($return['sop_params']['app_mid']);
+        $this->assertNotEmpty($return['sop_params']['time']);
+        $this->assertNotEmpty($return['sop_api_url']);
+        $this->assertNotEmpty($return['sop_point']);
+        $this->assertNotEmpty($return['sop_params']['sig']);
     }
 }
