9792e2a9d8ad06b179013f768510ea14d921b0b4
miaomiao7988 <miaomiao.zhang@d8aspring.com>
2016-04-07 16:28:37 +0800

    fix test

--- src/Wenwen/FrontendBundle/Tests/Controller/SurveyControllerTest.php
+++ src/Wenwen/FrontendBundle/Tests/Controller/SurveyControllerTest.php
@@ -5,7 +5,6 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
 use Doctrine\Common\DataFixtures\Loader;
-use Jili\ApiBundle\DataFixtures\ORM\LoadUserData;
 use Wenwen\FrontendBundle\Controller\SurveyController;
 
 class SurveyControllerTest extends WebTestCase
@@ -30,17 +29,12 @@ class SurveyControllerTest extends WebTestCase
         $executor = new ORMExecutor($em, $purger);
         $executor->purge();
 
-        $fixture = new LoadUserData();
-        $fixture->setContainer($container);
-
+        $fixture = new SurveyControllerTestFixture();
         $loader = new Loader();
         $loader->addFixture($fixture);
         $executor->execute($loader->getFixtures());
 
-        $this->container = $container;
         $this->em = $em;
-
-        @session_start();
     }
 
     /**
@@ -52,32 +46,35 @@ class SurveyControllerTest extends WebTestCase
         $this->em->close();
     }
 
+    public function testIndexPageWithoutLogin()
+    {
+        $client = static::createClient();
+        $container = $client->getContainer();
+        $url = $container->get('router')->generate('_survey_index');
+        $client->request('GET', $url);
+        $this->assertRegExp('/user\/login$/', $client->getResponse()->getTargetUrl());
+    }
+
     /**
      * @group dev-merge-ui-survey-list
      * @group dev-merge-ui-survey-top
      */
-    public function testIndexAction()
+    public function testIndexPageWithLogin()
     {
         $client = static::createClient();
         $container = $client->getContainer();
         $em = $this->em;
 
+        $url = $container->get('router')->generate('_login', array (), true);
+        $client->request('POST', $url, array (
+            'email' => 'user@voyagegroup.com.cn',
+            'pwd' => '11111q',
+            'remember_me' => '1'
+        ));
+        $client->followRedirect();
+
         $url = $container->get('router')->generate('_survey_index');
         $crawler = $client->request('GET', $url);
-
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-        $crawler = $client->followRedirect();
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-
-        //login 后
-        $session = $container->get('session');
-        $users = $em->getRepository('JiliApiBundle:User')->findAll();
-        $session->set('uid', $users[0]->getId());
-        $session->save();
-
-        $crawler = $client->request('GET', $url);
-        $this->assertEquals(301, $client->getResponse()->getStatusCode());
-        $crawler = $client->followRedirect();
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertTrue($crawler->filter('html:contains("问卷列表")')->count() > 0);
 
@@ -89,31 +86,34 @@ class SurveyControllerTest extends WebTestCase
         $this->assertCount(1, $crawler->filter('#sop_time'));
     }
 
+    public function testTopPageWithoutLogin()
+    {
+        $client = static::createClient();
+        $container = $client->getContainer();
+        $url = $container->get('router')->generate('_survey_top');
+        $client->request('GET', $url);
+        $this->assertRegExp('/user\/login$/', $client->getResponse()->getTargetUrl());
+    }
+
     /**
      * @group dev-merge-ui-survey-top
      */
-    public function testTopAction()
+    public function testTopPageWithLogin()
     {
         $client = static::createClient();
         $container = $client->getContainer();
         $em = $this->em;
 
+        $url = $container->get('router')->generate('_login', array (), true);
+        $crawler = $client->request('POST', $url, array (
+            'email' => 'user@voyagegroup.com.cn',
+            'pwd' => '11111q',
+            'remember_me' => '1'
+        ));
+        $client->followRedirect();
+
         $url = $container->get('router')->generate('_survey_top');
         $crawler = $client->request('GET', $url);
-
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-        $crawler = $client->followRedirect();
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-
-        //login 后
-        $session = $container->get('session');
-        $users = $em->getRepository('JiliApiBundle:User')->findAll();
-        $session->set('uid', $users[0]->getId());
-        $session->save();
-
-        $crawler = $client->request('GET', $url);
-        $this->assertEquals(301, $client->getResponse()->getStatusCode());
-        $crawler = $client->followRedirect();
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertCount(1, $crawler->filter('#sop_api_url'));
         $this->assertCount(1, $crawler->filter('#sop_point'));
@@ -144,5 +144,28 @@ class SurveyControllerTest extends WebTestCase
         $this->assertNotEmpty($return['sop_api_url']);
         $this->assertNotEmpty($return['sop_point']);
         $this->assertNotEmpty($return['sop_params']['sig']);
+    }
+}
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class SurveyControllerTestFixture implements FixtureInterface
+{
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new \Jili\ApiBundle\Entity\User();
+        $user->setNick(__CLASS__);
+        $user->setEmail('user@voyagegroup.com.cn');
+        $user->setPoints(100);
+        $user->setIsInfoSet(0);
+        $user->setIconPath('test/test_icon.jpg');
+        $user->setRewardMultiple(1);
+        $user->setPwd('11111q');
+        $user->setIsEmailConfirmed(1);
+        $user->setRegisterDate(new \DateTime());
+        $manager->persist($user);
+        $manager->flush();
     }
 }
