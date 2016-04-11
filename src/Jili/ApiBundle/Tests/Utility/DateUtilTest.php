c34f3adc0b2bfcb7c548ebc0e49ce7dd52fd3949
Takafumi Sekiguchi <takafumi.sekiguchi@gmail.com>
2016-04-11 14:22:50 +0800

    Add function to convert timezone

--- src/Jili/ApiBundle/Tests/Utility/DateUtilTest.php
+++ src/Jili/ApiBundle/Tests/Utility/DateUtilTest.php
@@ -1,13 +1,13 @@
 <?php
+
 namespace Jili\ApiBundle\Tests\Utility;
+
 use Jili\ApiBundle\Utility\DateUtil;
 
-class DateUtilTest extends\PHPUnit_Framework_TestCase {
-
-    /**
-    * @group GetTimeByMonth
-    */
-    public function testGetTimeByMonth() {
+class DateUtilTest extends\PHPUnit_Framework_TestCase
+{
+    public function testGetTimeByMonth()
+    {
         $year = date('Y');
         $date = DateUtil :: getTimeByMonth(9);
         $this->assertEquals($year.'-09-01 00:00:00', $date['start_time']);
@@ -16,5 +16,10 @@ class DateUtilTest extends\PHPUnit_Framework_TestCase {
         $date = DateUtil :: getTimeByMonth(8);
         $this->assertEquals($year.'-08-01 00:00:00', $date['start_time']);
         $this->assertEquals($year.'-08-31 23:59:59', $date['end_time']);
+    }
+
+    public function testConvertTimeZone()
+    {
+        $this->assertEquals('2016-01-15 11:00:00', DateUtil::convertTimeZone('2016-01-15 12:00:00', 'Asia/Tokyo', 'Asia/Shanghai'));
     }
 }
