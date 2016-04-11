0268145b5c66a31e9ba9f42147433c7fd967f84e
lillian <wlt_liting@163.com>
2016-04-11 21:28:24 +0800

    fix home js

--- web/js/app/home.js
+++ web/js/app/home.js
@@ -13,6 +13,9 @@ require(['../config'],function(){
     //});
     require(['countdown']);
     require(['jquery', 'jqueryCookie'], function($){
+        //读取cookie
+        var res = document.cookie.substring(5,10);
+        //如果没有cookie执行以下操作
         //新手引导部分
         if(res!="guide"){
             var omar = $('.main-con').height() + 385;
@@ -22,35 +25,19 @@ require(['../config'],function(){
                 var current = $(this).parent().parent();
                 current.hide();
                 current.next().show();
-        function shouldShow(){
-            var vp  = $.cookie('ShoudShowDialog92');
-            if (vp == undefined || vp == 1) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        var omar = $('.main-con').height() + 385;
-        $('#newguideWrap').css('margin-top','-'+ omar +'px')
-        $('#mask, #newguideWrap, #newguideWrap div:eq(0)').show();
-        $('#newguideWrap a.ngbtn').click(function(){
-            var current = $(this).parent().parent();
-            current.hide();
-            current.next().show();
-        });
+            });
 
-        $(document.body).click(function(event){
-            var target = $(event.target);
-            if(target.is('.ngbtn1, .ngbtn2')){ return false; }
-            $.cookie('ShoudShowDialog92', 0, { expires: 10000, path: '/' });
-            $('#mask, #newguideWrap').hide();
-        });
-        if(shouldShow()){
-            $('#mask, #newguideWrap').show();
-        }else{
-            $('#mask, #newguideWrap').hide();
+            $(document.body).click(function(event){
+                var target = $(event.target);
+                if(target.is('.ngbtn1, .ngbtn2')){ return false; }
+                $('#mask, #newguideWrap').hide();
+            });
+            //添加cookie
+            var oDate = new Date();
+            oDate.setDate(oDate.getDate() + 10000);
+            document.cookie="name=guide;expires=" + oDate;
         }
-    });  
+    });     
     require(['jquery', 'sopSurvey', 'backbone', 'routing','jqueryCookie'], function($, survey, backbone, routing) {
 
         var pop_survey_window = function(element) {
@@ -148,19 +135,23 @@ require(['../config'],function(){
                 });
             }
         };
+        
         var showSurveyOther1 = function(res){
+            var ssiSurveyLen = $('#SurveySSILen').val();
             if(res.data.cint_research.length != 0){
                 renderCintResearchItems(res.data.cint_research, 1);
             }else if(res.data.fulcrum_research.length != 0){
                 renderFulcrumResearchItems(res.data.fulcrum_research, 1);
             }else if(parseInt(ssiSurveyLen) != 0){
-                console.log('显示ssi问卷');
+                // console.log('show one ssi survey.');
             }else{
+                // console.log('no survey!');
                 $('#surveyHome').hide();
             }
-        }
+        };
 
         var showSurveyOther2 = function(res){
+            var ssiSurveyLen = $('#SurveySSILen').val();
             if(res.data.cint_research.length >= 2){
                 renderCintResearchItems(res.data.cint_research, 2);
                 return;
@@ -169,7 +160,7 @@ require(['../config'],function(){
                 if(res.data.fulcrum_research.length != 0){
                     renderFulcrumResearchItems(res.data.fulcrum_research, 1);
                 }else if(parseInt(ssiSurveyLen) != 0){
-                    console.log('显示一条ssi问卷');
+                    // console.log('show one ssi survey.');
                 }
                 return;
             }else{
@@ -178,31 +169,31 @@ require(['../config'],function(){
                 }else if(res.data.fulcrum_research.length == 1){
                     renderFulcrumResearchItems(res.data.fulcrum_research, 1);
                     if(parseInt(ssiSurveyLen) != 0){
-                        console.log('显示一条ssi问卷');
+                        // console.log('show one ssi survey.');
                     }
                 }else{
                     if(parseInt(ssiSurveyLen) >= 2){
-                        console.log('显示两条ssi问卷');
+                        // console.log('show two ssi survey.');
                     }else{
-                        console.log('木有问卷！');
+                        // console.log('no survey!');
+                        $('#surveyHome').hide();
                     }
                 }
             } 
-        }
+        };
 
         var showSurvey = function(res, curNum){
             switch(res.data.user_agreement.length){
                 case 0: 
                     var ssiAgreeLen = $('#UserAgreementSSILen').val();
                     if(parseInt(ssiAgreeLen) == 1){
-                        console.log('显示ssi同意问卷');
+                        console.log('show ssi user agreement survey.');
                         if(res.data.cint_research.length != 0){
                             renderCintResearchItems(res.data.cint_research, 1);
                         }else if(res.data.fulcrum_research.length != 0){
                             renderFulcrumResearchItems(res.data.fulcrum_research, 1);
                         }
                     }else{
-                        var ssiSurveyLen = $('#SurveySSILen').val();
                         if(curNum == 1){
                             showSurveyOther1(res);    
                         }else if(curNum == 0){
@@ -279,100 +270,108 @@ require(['../config'],function(){
             var callback = surveylistCallback;
             surveylistCallback = function(res){
                 dummy_res = { 
-                        'meta' : {'code': '200' },
-                        'data' : {
-                           "profiling": [
-                                   {
-                                       "url": "https://partners.surveyon.com.dev.researchpanelasia.com/resource/auth/v1_1?sig=2cec964cd9cd901d17725bd08131976a3ced393b160708fcce2d7767802023c5&next=%2Fprofile%2Fp%2Fq004&time=1438677550&app_id=25&sop_locale=&app_mid=13",
-                                       "name": "q004",
-                                       "title": "profiling"
-                                   }
-                            ],
-                           "research": [
+                    'meta' : {'code': '200' },
+                    'data' : {
+                       "profiling": [
                                {
-                                   "date": "2015-07-21",
-                                   "is_answered": "0",
-                                   "cpi": "0.00",
-                                   "is_closed": "0",
-                                   "ir": "0",
-                                   "extra_info": {
-                                       "point": {
-                                           "screenout": "30",
-                                           "quotafull": "30",
-                                           "complete": "670"
-                                       },
-                                       "date": {
-                                           "end_at": "2015-08-31 00:00:00",
-                                           "start_at": "2015-07-21 00:00:00"
-                                       },
-                                       "content": ""
-                                   },
-                                   "url": "https://partners.surveyon.com.dev.researchpanelasia.com/resource/auth/v1_1?sig=aaeca59caa406fff786976df7300ddc69992f75ffdbb4ea0616a868cf58062e5&next=%2Fproject_survey%2F393&time=1438677550&app_id=25&sop_locale=&app_mid=13",
-                                   "loi": "15",
-                                   "title": "Test 4",
-                                   "survey_id": "284",
-                                   "quota_id": "393"
+                                   "url": "https://partners.surveyon.com.dev.researchpanelasia.com/resource/auth/v1_1?sig=2cec964cd9cd901d17725bd08131976a3ced393b160708fcce2d7767802023c5&next=%2Fprofile%2Fp%2Fq004&time=1438677550&app_id=25&sop_locale=&app_mid=13",
+                                   "name": "q004",
+                                   "title": "profiling"
                                }
-                            ],
-                           'user_agreement':[
-                            ],
-                           'fulcrum_research':[
-                                {
-                                   "survey_id": "4",
-                                   "quota_id": "10",
-                                   "cpi": "0.00",
-                                   "ir": "80",
-                                   "loi": "10",
-                                   "title": "来自Fulcrum的调查问卷",
-                                   "url": "https://partners.surveyon.com/resource/auth/v1_1?sig=e523d747983fb8adcfd858b432bc7d15490fae8f5ccb16c75f8f72e86c37672b&next=%2Fproject_survey%2F23456&time=1416302209&app_id=22&app_mid=test2",
-                                   "date": "2015-01-01",
-                                   "extra_info": {
-                                       "point": {"complete": "10"}
+                        ],
+                       "research": [
+                           {
+                               "date": "2015-07-21",
+                               "is_answered": "0",
+                               "cpi": "0.00",
+                               "is_closed": "0",
+                               "ir": "0",
+                               "extra_info": {
+                                   "point": {
+                                       "screenout": "30",
+                                       "quotafull": "30",
+                                       "complete": "670"
+                                   },
+                                   "date": {
+                                       "end_at": "2015-08-31 00:00:00",
+                                       "start_at": "2015-07-21 00:00:00"
+                                   },
+                                   "content": ""
+                               },
+                               "url": "https://partners.surveyon.com.dev.researchpanelasia.com/resource/auth/v1_1?sig=aaeca59caa406fff786976df7300ddc69992f75ffdbb4ea0616a868cf58062e5&next=%2Fproject_survey%2F393&time=1438677550&app_id=25&sop_locale=&app_mid=13",
+                               "loi": "15",
+                               "title": "Test 4",
+                               "survey_id": "284",
+                               "quota_id": "393"
+                           }
+                        ],
+                       'user_agreement':[
+                            {
+                               "type": "Fulcrum",
+                               "url": "http://researchpanelasia.com"
+                             },
+                             {
+                               "type": "Cint",
+                               "url": "http://www.d8aspring.com"
+                             }
+                        ],
+                       'fulcrum_research':[
+                            {
+                               "survey_id": "4",
+                               "quota_id": "10",
+                               "cpi": "0.00",
+                               "ir": "80",
+                               "loi": "10",
+                               "title": "来自Fulcrum的调查问卷",
+                               "url": "https://partners.surveyon.com/resource/auth/v1_1?sig=e523d747983fb8adcfd858b432bc7d15490fae8f5ccb16c75f8f72e86c37672b&next=%2Fproject_survey%2F23456&time=1416302209&app_id=22&app_mid=test2",
+                               "date": "2015-01-01",
+                               "extra_info": {
+                                   "point": {"complete": "10"}
+                                }
+                            }
+                        ],
+                       "cint_research": [
+                            {
+                                "survey_id": "10000",
+                                "quota_id": "20000",
+                                "cpi": "0.00",
+                                "ir": "80",
+                                "loi": "10",
+                                "is_answered": "0",
+                                "is_closed": "0",
+                                "title": "Cint Survey",
+                                "url": "https://partners.surveyon.com/resource/auth/v1_1?sig=e523d747983fb8adcfd858b432bc7d15490fae8f5ccb16c75f8f72e86c37672b&next=%2Fproject_survey%2F23456&time=1416302209&app_id=22&app_mid=test2",
+                                "date": "2015-01-01",
+                                "extra_info": {
+                                    "point": {
+                                        "complete": "40",
+                                        "screenout": "10",
+                                        "quotafull": "10"
                                     }
                                 }
-                            ],
-                           "cint_research": [
-                                {
-                                    "survey_id": "10000",
-                                    "quota_id": "20000",
-                                    "cpi": "0.00",
-                                    "ir": "80",
-                                    "loi": "10",
-                                    "is_answered": "0",
-                                    "is_closed": "0",
-                                    "title": "Cint Survey",
-                                    "url": "https://partners.surveyon.com/resource/auth/v1_1?sig=e523d747983fb8adcfd858b432bc7d15490fae8f5ccb16c75f8f72e86c37672b&next=%2Fproject_survey%2F23456&time=1416302209&app_id=22&app_mid=test2",
-                                    "date": "2015-01-01",
-                                    "extra_info": {
-                                        "point": {
-                                            "complete": "40",
-                                            "screenout": "10",
-                                            "quotafull": "10"
-                                        }
-                                    }
-                                },
-                                {
-                                    "survey_id": "10002",
-                                    "quota_id": "20000",
-                                    "cpi": "0.00",
-                                    "ir": "80",
-                                    "loi": "10",
-                                    "is_answered": "1",
-                                    "is_closed": "0",
-                                    "title": "Cint Survey2",
-                                    "url": "https://partners.surveyon.com/resource/auth/v1_1?sig=e523d747983fb8adcfd858b432bc7d15490fae8f5ccb16c75f8f72e86c37672b&next=%2Fproject_survey%2F23456&time=1416302209&app_id=22&app_mid=test2",
-                                    "date": "2015-01-01",
-                                    "extra_info": {
-                                        "point": {
-                                            "complete": "40",
-                                            "screenout": "10",
-                                            "quotafull": "10"
-                                        }
+                            },
+                            {
+                                "survey_id": "10002",
+                                "quota_id": "20000",
+                                "cpi": "0.00",
+                                "ir": "80",
+                                "loi": "10",
+                                "is_answered": "1",
+                                "is_closed": "0",
+                                "title": "Cint Survey2",
+                                "url": "https://partners.surveyon.com/resource/auth/v1_1?sig=e523d747983fb8adcfd858b432bc7d15490fae8f5ccb16c75f8f72e86c37672b&next=%2Fproject_survey%2F23456&time=1416302209&app_id=22&app_mid=test2",
+                                "date": "2015-01-01",
+                                "extra_info": {
+                                    "point": {
+                                        "complete": "40",
+                                        "screenout": "10",
+                                        "quotafull": "10"
                                     }
                                 }
-                            ]
-                        }
-                    };
+                            }
+                        ]
+                    }
+                };
                 callback(dummy_res);
             };
         }
@@ -381,5 +380,5 @@ require(['../config'],function(){
         if (1){
             mockResponse();
         }
-    });  
+    });
 });
