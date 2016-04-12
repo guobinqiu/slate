5cb02a27d3d71dd49b7a4dd4bccb23c690c7727c
lillian <wlt_liting@163.com>
2016-04-12 11:42:45 +0800

    update surveyList js and add pictures

--- web/js/app/survey/surveyList.js
+++ web/js/app/survey/surveyList.js
@@ -238,7 +238,7 @@ require(['../../config'], function() {
         }
         // preview
         var $preview = $('#preview').val();
-        if (1){
+        if ($preview){
             mockResponse();
         }
     });
