ae0033b8b9ad0183bbaf61c450028cde1117e936
lillian <wlt_liting@163.com>
2016-05-10 12:03:08 +0800

    update iframe src

--- web/js/app/advertisement/advertisement.js
+++ web/js/app/advertisement/advertisement.js
@@ -23,10 +23,12 @@ require(['../../config'],function(){
         }
         if(window.location.hostname == 'www.91wenwen.net'){
             if($('.offerWow').length >= 1){
-                $('iframe').attr('src', 'http://www.offer-wow.com/affiliate/wall/open.do?websiteid=1162&styleIndex=1&memberid=1132938').add('.taskFlow').show();    
+                var websiteid = $('#websiteid').val(), memberid = $('#websiteid').val();
+                $('iframe').attr('src', 'http://www.offer-wow.com/affiliate/wall/open.do?websiteid='+websiteid +'&styleIndex=1&memberid='+memberid).add('.taskFlow').show();    
             }
             if($('.offer99').length >= 1){
-                $('iframe').attr('src', 'http://app.offer99.com/index.php?pid=f4b42b65ce528bb151ea6e2eb5a01692&userid=1132938').add('.taskFlow').show();    
+                var pid = $('#pid').val(), userid = $('#userid').val();
+                $('iframe').attr('src', 'http://app.offer99.com/index.php?pid='+pid +'&userid='+userid).add('.taskFlow').show();    
             }
             $('.maintenance').hide();
         }else{
