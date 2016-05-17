49124854c3671e6b6a346d156fabd4e25178814c
xiaoyi.chai <xiaoyi.chai@d8aspring.com>
2016-05-17 15:13:51 +0800

    Add cost of CPS/CPA and expense of register

--- scripts/perl/Lib/Wenwen/Model/Service/PointsSummary.pm
+++ scripts/perl/Lib/Wenwen/Model/Service/PointsSummary.pm
@@ -1,6 +1,11 @@
 package Wenwen::Model::Service::PointsSummary;
 use common::sense;
 
+##
+#   costs
+##
+
+# research survey cost
 sub sum_survey_cost_points {
     my ($class, $handle, $from, $to) = @_;
     my $dbh = $handle->dbh;
@@ -27,6 +32,146 @@ sub sum_survey_cost_points {
     return $total_sum;
 }
 
+# cost for CPS - Chanet
+sub sum_cps_chanet_cost_points {
+    my ($class, $handle, $from, $to) = @_;
+    my $dbh = $handle->dbh;
+
+    my $total_sum = 0;
+    my $counter   = 0;
+    while ($counter < 10) {
+        my $sql = "
+            SELECT
+                sum(p.point_change_num)
+            FROM point_history0${counter} p 
+            WHERE 
+                p.reason = 2
+                AND p.create_time >= ?
+                AND p.create_time < ?
+        ";
+
+        my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+        $sth->execute($from->strftime('%F %T'), $to->strftime('%F %T')) or die $dbh->errstr;
+        my $sum = $sth->fetchall_arrayref()->[0][0];
+        $total_sum += $sum;
+        $counter++;
+    }
+    return $total_sum;
+}
+
+# cost for CPS - Emar
+sub sum_cps_emar_cost_points {
+    my ($class, $handle, $from, $to) = @_;
+    my $dbh = $handle->dbh;
+
+    my $total_sum = 0;
+    my $counter   = 0;
+    while ($counter < 10) {
+        my $sql = "
+            SELECT
+                sum(p.point_change_num)
+            FROM point_history0${counter} p 
+            WHERE 
+                (p.reason = 19 or p.reason = 20)
+                AND p.create_time >= ?
+                AND p.create_time < ?
+        ";
+
+        my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+        $sth->execute($from->strftime('%F %T'), $to->strftime('%F %T')) or die $dbh->errstr;
+        my $sum = $sth->fetchall_arrayref()->[0][0];
+        $total_sum += $sum;
+        $counter++;
+    }
+    return $total_sum;
+}
+
+# cost for CPS - Duomai
+sub sum_cps_duomai_cost_points {
+    my ($class, $handle, $from, $to) = @_;
+    my $dbh = $handle->dbh;
+
+    my $total_sum = 0;
+    my $counter   = 0;
+    while ($counter < 10) {
+        my $sql = "
+            SELECT
+                sum(p.point_change_num)
+            FROM point_history0${counter} p 
+            WHERE 
+                p.reason = 23
+                AND p.create_time >= ?
+                AND p.create_time < ?
+        ";
+
+        my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+        $sth->execute($from->strftime('%F %T'), $to->strftime('%F %T')) or die $dbh->errstr;
+        my $sum = $sth->fetchall_arrayref()->[0][0];
+        $total_sum += $sum;
+        $counter++;
+    }
+    return $total_sum;
+}
+
+# cost for CPA - offer99
+sub sum_cpa_offer99_cost_points {
+    my ($class, $handle, $from, $to) = @_;
+    my $dbh = $handle->dbh;
+
+    my $total_sum = 0;
+    my $counter   = 0;
+    while ($counter < 10) {
+        my $sql = "
+            SELECT
+                sum(p.point_change_num)
+            FROM point_history0${counter} p 
+            WHERE 
+                p.reason = 18
+                AND p.create_time >= ?
+                AND p.create_time < ?
+        ";
+
+        my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+        $sth->execute($from->strftime('%F %T'), $to->strftime('%F %T')) or die $dbh->errstr;
+        my $sum = $sth->fetchall_arrayref()->[0][0];
+        $total_sum += $sum;
+        $counter++;
+    }
+    return $total_sum;
+}
+
+# cost for CPA - offerwow
+sub sum_cpa_offerwow_cost_points {
+    my ($class, $handle, $from, $to) = @_;
+    my $dbh = $handle->dbh;
+
+    my $total_sum = 0;
+    my $counter   = 0;
+    while ($counter < 10) {
+        my $sql = "
+            SELECT
+                sum(p.point_change_num)
+            FROM point_history0${counter} p 
+            WHERE 
+                p.reason = 17
+                AND p.create_time >= ?
+                AND p.create_time < ?
+        ";
+
+        my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+        $sth->execute($from->strftime('%F %T'), $to->strftime('%F %T')) or die $dbh->errstr;
+        my $sum = $sth->fetchall_arrayref()->[0][0];
+        $total_sum += $sum;
+        $counter++;
+    }
+    return $total_sum;
+}
+
+##
+#   expenses
+##
+
+# expense for quickpoll and profiling survey
 sub sum_survey_expense_points {
     my ($class, $handle, $from, $to) = @_;
     my $dbh = $handle->dbh;
@@ -53,6 +198,36 @@ sub sum_survey_expense_points {
     return $total_sum;
 }
 
+# expense for register
+sub sum_register_expense_points {
+    my ($class, $handle, $from, $to) = @_;
+    my $dbh = $handle->dbh;
+
+    my $total_sum = 0;
+    my $counter   = 0;
+    while ($counter < 10) {
+        my $sql = "
+            SELECT
+                sum(p.point_change_num)
+            FROM point_history0${counter} p 
+            WHERE 
+                p.reason = 32
+                AND p.create_time >= ?
+                AND p.create_time < ?
+        ";
+
+        my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+        $sth->execute($from->strftime('%F %T'), $to->strftime('%F %T')) or die $dbh->errstr;
+        my $sum = $sth->fetchall_arrayref()->[0][0];
+        $total_sum += $sum;
+        $counter++;
+    }
+    return $total_sum;
+}
+
+##
+#   expired points
+##
 sub sum_expired_points {
     my ($class, $handle, $from, $to) = @_;
     my $dbh = $handle->dbh;
@@ -79,6 +254,11 @@ sub sum_expired_points {
     return $total_sum;
 }
 
+##
+#  exchanged points
+##
+
+# exchanged to alipay
 sub sum_exchanged_alipay_points {
     my ($class, $handle, $from, $to) = @_;
     my $dbh = $handle->dbh;
@@ -105,6 +285,7 @@ sub sum_exchanged_alipay_points {
     return $total_sum;
 }
 
+# exchanged to mobile fee
 sub sum_exchanged_mobile_fee_points {
     my ($class, $handle, $from, $to) = @_;
     my $dbh = $handle->dbh;
@@ -120,6 +301,146 @@ sub sum_exchanged_mobile_fee_points {
                 p.reason = 12
                 AND p.create_time >= ?
                 AND p.create_time < ?
+        ";
+
+        my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+        $sth->execute($from->strftime('%F %T'), $to->strftime('%F %T')) or die $dbh->errstr;
+        my $sum = $sth->fetchall_arrayref()->[0][0];
+        $total_sum += $sum;
+        $counter++;
+    }
+    return $total_sum;
+}
+
+##
+#   Order Cost - order happened but not confirmed yet
+##
+
+# order cost for CPS - Chanet
+sub sum_cps_chanet_order_cost_points {
+    my ($class, $handle, $from, $to) = @_;
+    my $dbh = $handle->dbh;
+
+    my $total_sum = 0;
+    my $counter   = 0;
+    while ($counter < 10) {
+        my $sql = "
+            SELECT
+                sum(t.point)
+            FROM task_history0${counter} t 
+            WHERE 
+                t.status >= 2 
+                AND t.category_type = 2
+                AND t.ocd_created_date >= ?
+                AND t.ocd_created_date < ?
+        ";
+
+        my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+        $sth->execute($from->strftime('%F %T'), $to->strftime('%F %T')) or die $dbh->errstr;
+        my $sum = $sth->fetchall_arrayref()->[0][0];
+        $total_sum += $sum;
+        $counter++;
+    }
+    return $total_sum;
+}
+
+# order cost for CPS - Emar
+sub sum_cps_emar_order_cost_points {
+    my ($class, $handle, $from, $to) = @_;
+    my $dbh = $handle->dbh;
+
+    my $total_sum = 0;
+    my $counter   = 0;
+    while ($counter < 10) {
+        my $sql = "
+            SELECT
+                sum(t.point)
+            FROM task_history0${counter} t 
+            WHERE 
+                (t.category_type = 19 or t.category_type = 20) 
+                AND t.ocd_created_date >= ?
+                AND t.ocd_created_date < ?
+        ";
+
+        my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+        $sth->execute($from->strftime('%F %T'), $to->strftime('%F %T')) or die $dbh->errstr;
+        my $sum = $sth->fetchall_arrayref()->[0][0];
+        $total_sum += $sum;
+        $counter++;
+    }
+    return $total_sum;
+}
+
+# order cost for CPS - Duomai
+sub sum_cps_duomai_order_cost_points {
+    my ($class, $handle, $from, $to) = @_;
+    my $dbh = $handle->dbh;
+
+    my $total_sum = 0;
+    my $counter   = 0;
+    while ($counter < 10) {
+        my $sql = "
+            SELECT
+                sum(t.point)
+            FROM task_history0${counter} t 
+            WHERE 
+                t.category_type = 23
+                AND t.ocd_created_date >= ?
+                AND t.ocd_created_date < ?
+        ";
+
+        my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+        $sth->execute($from->strftime('%F %T'), $to->strftime('%F %T')) or die $dbh->errstr;
+        my $sum = $sth->fetchall_arrayref()->[0][0];
+        $total_sum += $sum;
+        $counter++;
+    }
+    return $total_sum;
+}
+
+# order cost for CPA - offer99
+sub sum_cpa_offer99_order_cost_points {
+    my ($class, $handle, $from, $to) = @_;
+    my $dbh = $handle->dbh;
+
+    my $total_sum = 0;
+    my $counter   = 0;
+    while ($counter < 10) {
+        my $sql = "
+            SELECT
+                sum(t.point)
+            FROM task_history0${counter} t 
+            WHERE 
+                t.category_type = 18
+                AND t.ocd_created_date >= ?
+                AND t.ocd_created_date < ?
+        ";
+
+        my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+        $sth->execute($from->strftime('%F %T'), $to->strftime('%F %T')) or die $dbh->errstr;
+        my $sum = $sth->fetchall_arrayref()->[0][0];
+        $total_sum += $sum;
+        $counter++;
+    }
+    return $total_sum;
+}
+
+# order cost for CPA - offerwow
+sub sum_cpa_offerwow_order_cost_points {
+    my ($class, $handle, $from, $to) = @_;
+    my $dbh = $handle->dbh;
+
+    my $total_sum = 0;
+    my $counter   = 0;
+    while ($counter < 10) {
+        my $sql = "
+            SELECT
+                sum(t.point)
+            FROM task_history0${counter} t 
+            WHERE 
+                t.category_type = 17
+                AND t.ocd_created_date >= ?
+                AND t.ocd_created_date < ?
         ";
 
         my $sth = $dbh->prepare($sql) or die $dbh->errstr;
--- scripts/perl/Lib/Wenwen/Task/GetPointsReport.pm
+++ scripts/perl/Lib/Wenwen/Task/GetPointsReport.pm
@@ -70,29 +70,82 @@ sub do_task {
     my $self = shift;
     my $buff = "\n";
 
-    my $sum_survey_cost_points          = $self->get_survey_cost_points();
-    my $sum_survey_expense_points       = $self->get_survey_expense_points();
-    my $sum_expired_points              = $self->get_expired_points();
+    my $sum_survey_cost_points       = $self->get_survey_cost_points();
+    my $sum_cps_chanet_cost_points   = $self->get_cps_chanet_cost_points();
+    my $sum_cps_emar_cost_points     = $self->get_cps_emar_cost_points();
+    my $sum_cps_duomai_cost_points   = $self->get_cps_duomai_cost_points();
+    my $sum_cpa_offer99_cost_points  = $self->get_cpa_offer99_cost_points();
+    my $sum_cpa_offerwow_cost_points = $self->get_cpa_offerwow_cost_points();
+
+    my $total_cps_cost_points
+        = $sum_cps_chanet_cost_points + $sum_cps_emar_cost_points + $sum_cps_duomai_cost_points;
+    my $total_cpa_cost_points = $sum_cpa_offer99_cost_points + $sum_cpa_offerwow_cost_points;
+    my $total_cost_points
+        = $sum_survey_cost_points + $total_cps_cost_points + $total_cpa_cost_points;
+
+    my $sum_survey_expense_points   = $self->get_survey_expense_points();
+    my $sum_register_expense_points = $self->get_register_expense_points();
+
+    my $total_expense_points = $sum_register_expense_points;
+
+    my $sum_expired_points = $self->get_expired_points();
+
     my $sum_exchanged_alipay_points     = $self->get_exchanged_alipay_points();
     my $sum_exchanged_mobile_fee_points = $self->get_exchanged_mobile_fee_points();
+    my $total_exchanged_points = $sum_exchanged_alipay_points + $sum_exchanged_mobile_fee_points;
 
-    $buff = $buff . sprintf "Report from %s to %s \n\n", $self->from_date->strftime('%F %T'),
+    my $sum_cps_chanet_order_cost_points   = $self->get_cps_chanet_order_cost_points();
+    my $sum_cps_emar_order_cost_points     = $self->get_cps_emar_order_cost_points();
+    my $sum_cps_duomai_order_cost_points   = $self->get_cps_duomai_order_cost_points();
+    my $sum_cpa_offer99_order_cost_points  = $self->get_cpa_offer99_order_cost_points();
+    my $sum_cpa_offerwow_order_cost_points = $self->get_cpa_offerwow_order_cost_points();
+
+    $buff = $buff . sprintf "Points Report from %s to %s \n", $self->from_date->strftime('%F %T'),
         $self->to_date->strftime('%F %T');
+    $buff = $buff . sprintf "\n";
     $buff = $buff . sprintf "-------------------------- \n";
-    $buff = $buff . sprintf "Points Summary \n";
+    $buff = $buff . sprintf "Summary \n";
+    $buff = $buff . sprintf "-------------------------- \n";
+    $buff = $buff . sprintf "Total Cost Points. \n=> %d\n", $total_cost_points;
+    $buff = $buff . sprintf "Total Expense Points. \n=> %d\n", $total_expense_points;
+    $buff = $buff . sprintf "Total Expired Points. \n=> %d\n", $sum_expired_points;
+    $buff = $buff . sprintf "Total Exchanged Points. \n=> %d\n", $total_exchanged_points;
+    $buff = $buff . sprintf "\n";
+    $buff = $buff . sprintf "-------------------------- \n";
+    $buff = $buff . sprintf "Cost Details\n";
     $buff = $buff . sprintf "-------------------------- \n";
     $buff = $buff . sprintf "Survey Cost Points. \n=> %d\n", $sum_survey_cost_points;
+    $buff = $buff . sprintf "CPS chanet Cost Points. \n=> %d\n", $sum_cps_chanet_cost_points;
+    $buff = $buff . sprintf "CPS emar Cost Points. \n=> %d\n", $sum_cps_emar_cost_points;
+    $buff = $buff . sprintf "CPS duomai Cost Points. \n=> %d\n", $sum_cps_duomai_cost_points;
+    $buff = $buff . sprintf "CPA offer99 Cost Points. \n=> %d\n", $sum_cpa_offer99_cost_points;
+    $buff = $buff . sprintf "CPA offerwow Cost Points. \n=> %d\n", $sum_cpa_offerwow_cost_points;
+    $buff = $buff . sprintf "\n";
+    $buff = $buff . sprintf "-------------------------- \n";
+    $buff = $buff . sprintf "Expense Details\n";
+    $buff = $buff . sprintf "-------------------------- \n";
     $buff = $buff . sprintf "Survey Expense Points. \n=> %d\n", $sum_survey_expense_points;
-    $buff = $buff . sprintf "Expired Points. \n=> %d\n", $sum_expired_points;
-    $buff = $buff . sprintf "Total Exchanged Points. \n=> %d\n\n",
-        $sum_exchanged_alipay_points + $sum_exchanged_mobile_fee_points;
-
+    $buff = $buff . sprintf "Register Expense Points. \n=> %d\n", $sum_register_expense_points;
+    $buff = $buff . sprintf "\n";
     $buff = $buff . sprintf "-------------------------- \n";
     $buff = $buff . sprintf "Exchanged Details \n";
     $buff = $buff . sprintf "-------------------------- \n";
     $buff = $buff . sprintf "Alipay. \n=> %d\n", $sum_exchanged_alipay_points;
     $buff = $buff . sprintf "Mobile Fee. \n=> %d\n", $sum_exchanged_mobile_fee_points;
-
+    $buff = $buff . sprintf "\n";
+    $buff = $buff . sprintf "-------------------------- \n";
+    $buff = $buff . sprintf "Order Cost Details\n";
+    $buff = $buff . sprintf "-------------------------- \n";
+    $buff = $buff . sprintf "CPS chanet Order Cost Points. \n=> %d\n",
+        $sum_cps_chanet_order_cost_points;
+    $buff = $buff . sprintf "CPS emar Order Cost Points. \n=> %d\n",
+        $sum_cps_emar_order_cost_points;
+    $buff = $buff . sprintf "CPS duomai Order Cost Points. \n=> %d\n",
+        $sum_cps_duomai_order_cost_points;
+    $buff = $buff . sprintf "CPA offer99 Order Cost Points. \n=> %d\n",
+        $sum_cpa_offer99_order_cost_points;
+    $buff = $buff . sprintf "CPA offerwow Order Cost Points. \n=> %d\n",
+        $sum_cpa_offerwow_order_cost_points;
     $buff = $buff . sprintf "\n";
 
     return $buff;
@@ -111,6 +164,66 @@ sub get_survey_cost_points {
 }
 
 ##
+#   Cost Points for CPS - Chanet
+##
+sub get_cps_chanet_cost_points {
+    my $self = shift;
+
+    my $cps_chanet_cost_points
+        = Wenwen::Model::Service::PointsSummary->sum_cps_chanet_cost_points($handle,
+        $self->from_date, $self->to_date);
+    return $cps_chanet_cost_points;
+}
+
+##
+#   Cost Points for CPS - Emar
+##
+sub get_cps_emar_cost_points {
+    my $self = shift;
+
+    my $cps_emar_cost_points
+        = Wenwen::Model::Service::PointsSummary->sum_cps_emar_cost_points($handle,
+        $self->from_date, $self->to_date);
+    return $cps_emar_cost_points;
+}
+
+##
+#   Cost Points for CPS - Duomai
+##
+sub get_cps_duomai_cost_points {
+    my $self = shift;
+
+    my $cps_duomai_cost_points
+        = Wenwen::Model::Service::PointsSummary->sum_cps_duomai_cost_points($handle,
+        $self->from_date, $self->to_date);
+    return $cps_duomai_cost_points;
+}
+
+##
+#   Cost Points for CPA - offer99
+##
+sub get_cpa_offer99_cost_points {
+    my $self = shift;
+
+    my $cpa_offer99_cost_points
+        = Wenwen::Model::Service::PointsSummary->sum_cpa_offer99_cost_points($handle,
+        $self->from_date, $self->to_date);
+    return $cpa_offer99_cost_points;
+}
+
+##
+#   Cost Points for CPA - offerwow
+##
+sub get_cpa_offerwow_cost_points {
+    my $self = shift;
+
+    my $cpa_offerwow_cost_points
+        = Wenwen::Model::Service::PointsSummary->sum_cpa_offerwow_cost_points($handle,
+        $self->from_date, $self->to_date);
+    return $cpa_offerwow_cost_points;
+}
+
+##
 #   Survey Expense Points
 ##
 sub get_survey_expense_points {
@@ -118,6 +231,18 @@ sub get_survey_expense_points {
 
     my $survey_expense_points
         = Wenwen::Model::Service::PointsSummary->sum_survey_expense_points($handle,
+        $self->from_date, $self->to_date);
+    return $survey_expense_points;
+}
+
+##
+#   Register Expense Points
+##
+sub get_register_expense_points {
+    my $self = shift;
+
+    my $survey_expense_points
+        = Wenwen::Model::Service::PointsSummary->sum_register_expense_points($handle,
         $self->from_date, $self->to_date);
     return $survey_expense_points;
 }
@@ -156,6 +281,66 @@ sub get_exchanged_mobile_fee_points {
         = Wenwen::Model::Service::PointsSummary->sum_exchanged_mobile_fee_points($handle,
         $self->from_date, $self->to_date);
     return $exchanged_alipay_points;
+}
+
+##
+#   Order Cost Points for CPS - Chanet
+##
+sub get_cps_chanet_order_cost_points {
+    my $self = shift;
+
+    my $cps_chanet_cost_points
+        = Wenwen::Model::Service::PointsSummary->sum_cps_chanet_order_cost_points($handle,
+        $self->from_date, $self->to_date);
+    return $cps_chanet_cost_points;
+}
+
+##
+#   Order Cost Points for CPS - Emar
+##
+sub get_cps_emar_order_cost_points {
+    my $self = shift;
+
+    my $cps_emar_cost_points
+        = Wenwen::Model::Service::PointsSummary->sum_cps_emar_order_cost_points($handle,
+        $self->from_date, $self->to_date);
+    return $cps_emar_cost_points;
+}
+
+##
+#   Order Cost Points for CPS - Duomai
+##
+sub get_cps_duomai_order_cost_points {
+    my $self = shift;
+
+    my $cps_duomai_cost_points
+        = Wenwen::Model::Service::PointsSummary->sum_cps_duomai_order_cost_points($handle,
+        $self->from_date, $self->to_date);
+    return $cps_duomai_cost_points;
+}
+
+##
+#   Order Cost Points for CPA - offer99
+##
+sub get_cpa_offer99_order_cost_points {
+    my $self = shift;
+
+    my $cpa_offer99_cost_points
+        = Wenwen::Model::Service::PointsSummary->sum_cpa_offer99_order_cost_points($handle,
+        $self->from_date, $self->to_date);
+    return $cpa_offer99_cost_points;
+}
+
+##
+#   Order Cost Points for CPA - offerwow
+##
+sub get_cpa_offerwow_order_cost_points {
+    my $self = shift;
+
+    my $cpa_offerwow_cost_points
+        = Wenwen::Model::Service::PointsSummary->sum_cpa_offerwow_order_cost_points($handle,
+        $self->from_date, $self->to_date);
+    return $cpa_offerwow_cost_points;
 }
 
 1;
