38b19d1af78f00f3b06ee888c947dd05dec458dc
xiaoyi.chai <xiaoyi.chai@d8aspring.com>
2016-05-26 16:39:27 +0800

    delete web_old dir

--- web_old/js/adListNew.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$(".explain .explainBtn").hover(
-	function(){
-		$(".explain .explainMore").show();
-	},function(){
-		$(".explain .explainMore").hide();
-	});
-});
