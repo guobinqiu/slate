048bff44994f9acbc613faf4733dc1f500788ba4
lillian <wlt_liting@163.com>
2016-06-13 14:35:51 +0800

    add login style

--- web/js/app/index.js
+++ web/js/app/index.js
@@ -8,70 +8,66 @@ require(['../config'],function(){
     //         animateTimer: 5000
     //     }});
     // });
-    // require(['jquery', 'loginForm'], function($, loginForm){
-    //     var loginPwd = {
-    //         ele: '#pwd',
-    //         prompt: {
-    //             isNull: '请输入您的密码',
-    //             isFocus: '请输入您的密码'
-    //         }
-    //     }, loginEmail = {
-    //         ele: '#email',
-    //         prompt: {
-    //             isNull: '请输入邮箱地址',
-    //             isFormat: '邮箱地址格式不正确'
-    //         },
-    //         type: 'email'
-    //     };
-    //     new loginForm({pwd: loginPwd, email: loginEmail, auto: false});
-    //     var submitBtn = $("#submit_button");
-    //     submitBtn.on('click', function(e){
-    //         var loginform = new loginForm({pwd: loginPwd, email: loginEmail, auto: true});
-    //         if(loginform.run(true)){
-    //             submitBtn.submit();
-    //         }else{
-    //             e.preventDefault();
-    //         }
-    //     });
+    require(['jquery', 'loginForm'], function($, loginForm){
+        var loginPwd = {
+            ele: '#pwd',
+            prompt: {
+                isNull: '请输入您的密码',
+                isFocus: '请输入您的密码'
+            }
+        }, loginEmail = {
+            ele: '#email',
+            prompt: {
+                isNull: '请输入邮箱地址',
+                isFormat: '邮箱地址格式不正确'
+            },
+            type: 'email'
+        };
+        new loginForm({pwd: loginPwd, email: loginEmail, auto: false});
+        var submitBtn = $("#submit_button");
+        submitBtn.on('click', function(e){
+            var loginform = new loginForm({pwd: loginPwd, email: loginEmail, auto: true});
+            if(loginform.run(true)){
+                submitBtn.submit();
+            }else{
+                e.preventDefault();
+            }
+        });
 
-    //     var $emailError = $("#email_error"), $pwdError = $("#pwd_error");
-    //     $emailError.add($pwdError).on('click', function(){
-    //         $(this).addClass('fade');
-    //     });
-    //     var errorCode = $('#error_code').val();
-    //     if(errorCode != undefined){
-    //         $emailError.html(errorCode).addClass('error').attr('display', 'block');
-    //     };
-    //     // 点击登录按钮，呈现输入状态
-    //     var logFoc = $("a[title='登录']");
-    //     var surList = $("a[title='问卷列表']");
-    //     logFoc.add(surList).click(function(){
-    //         $('#email').focus(); 
-    //     });
+        var $emailError = $("#email_error"), $pwdError = $("#pwd_error");
+        $emailError.add($pwdError).on('click', function(){
+            $(this).addClass('fade');
+        });
+        var errorCode = $('#error_code').val();
+        if(errorCode != undefined){
+            $emailError.html(errorCode).addClass('error').attr('display', 'block');
+        };
+        // 点击登录按钮，呈现输入状态
+        var logFoc = $("a[title='登录']");
+        var surList = $("a[title='问卷列表']");
+        logFoc.add(surList).click(function(){
+            $('#email').focus(); 
+        });
 
-    //     //sinaWeibo and QQ quick login prompt
-    //     var wbLog = $('.weibo-login');
-    //     var qqLog = $('.qq-login');
-    //     var wqClose = $('.quickLCon .closeBtn').add('.quickLCon .cancelBtn');
-    //     var wbPCon = $('#wbLogCon');
-    //     var qqPCon = $('#qqLogCon');
-    //     wbLog.on('click', function(){
-    //         wbPCon.show();
-    //         qqPCon.hide();
-    //     });
-    //     qqLog.on('click', function(){
-    //         qqPCon.show();
-    //         wbPCon.hide();
-    //     });
-    //     wqClose.on('click', function(){
-    //         wbPCon.add(qqPCon).hide();
-    //     });
+        //sinaWeibo and QQ quick login prompt
+        var wbLog = $('.weibo-login');
+        var qqLog = $('.qq-login');
+        var wqClose = $('.quickLCon .closeBtn').add('.quickLCon .cancelBtn');
+        var wbPCon = $('#wbLogCon');
+        var qqPCon = $('#qqLogCon');
+        wbLog.on('click', function(){
+            wbPCon.show();
+            qqPCon.hide();
+        });
+        qqLog.on('click', function(){
+            qqPCon.show();
+            wbPCon.hide();
+        });
+        wqClose.on('click', function(){
+            wbPCon.add(qqPCon).hide();
+        });
 
-    //     //点击问卷列表提示先登录
-    //     $('.surUnlog').on('click', function(){
-    //         $('.logFirst').fadeIn().delay(4000).fadeOut();
-    //     });
-    // });
+    });
     // require(['jquery'], function($){
     //     var $window = $(window),
     //         win_height_padded = $window.height() * 1.1;
