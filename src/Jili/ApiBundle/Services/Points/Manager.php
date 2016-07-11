3e10e75f53a1581e2c69fb08e3008e65a447761d
xiaoyi.chai <xiaoyi.chai@d8aspring.com>
2016-07-11 16:36:16 +0800

    变量命名规范化

--- src/Jili/ApiBundle/Services/Points/Manager.php
+++ src/Jili/ApiBundle/Services/Points/Manager.php
@@ -13,18 +13,18 @@ class Manager
     /**
      * 更新point: user, point_history , task_history
      */
-    public function updatePoints($user_id, $point,  $ad_category_id, $task_type_id, $task_name)
+    public function updatePoints($userId, $point,  $adCategoryId, $taskTypeId, $taskName)
     {
 
         $em = $this->em;//getDoctrine()->getManager();
-        $user = $em->getRepository('JiliApiBundle:User')->find($user_id);
+        $user = $em->getRepository('JiliApiBundle:User')->find($userId);
 
         if( ! isset($user ) ) {
-            $this->logger->info('UpdatePoints , user not exists user.id='.var_export($user_id,true));
+            $this->logger->info('UpdatePoints , user not exists user.id='.var_export($userId,true));
             return ;
         }
 
-        $update_time = date_create(date('Y-m-d H:i:s'));
+        $updateTime = date_create(date('Y-m-d H:i:s'));
 
         // transaction start
         $dbh = $em->getConnection();
@@ -37,25 +37,26 @@ class Manager
             $user->setPoints(intval($old_point + $point));
 
             // Create new object of point_history0x
-            $classPointHistory = 'Jili\ApiBundle\Entity\PointHistory0'. ( $user_id % 10);
+            $classPointHistory = 'Jili\ApiBundle\Entity\PointHistory0'. ( $userId % 10);
             $pointHistory = new $classPointHistory();
-            $pointHistory->setUserId($user_id);
+            $pointHistory->setUserId($userId);
             $pointHistory->setPointChangeNum($point);
-            $pointHistory->setReason($ad_category_id);
+            $pointHistory->setReason($adCategoryId);
 
             // Create new object of task_history0x
-            $classTaskHistory = 'Jili\ApiBundle\Entity\TaskHistory0'. ( $user_id % 10);
+            $classTaskHistory = 'Jili\ApiBundle\Entity\TaskHistory0'. ( $userId % 10);
             $taskHistory = new $classTaskHistory();
-            $taskHistory->setUserid($user_id);
+            $taskHistory->setUserid($userId);
             $taskHistory->setOrderId(0);
-            $taskHistory->setOcdCreatedDate($update_time);
-            $taskHistory->setCategoryType($ad_category_id);
-            $taskHistory->setTaskType($task_type_id);
-            $taskHistory->setTaskName($task_name);
-            $taskHistory->setDate($update_time);
+            $taskHistory->setOcdCreatedDate($updateTime);
+            $taskHistory->setCategoryType($adCategoryId);
+            $taskHistory->setTaskType($taskTypeId);
+            $taskHistory->setTaskName($taskName);
+            $taskHistory->setDate($updateTime);
             $taskHistory->setPoint($point);
             $taskHistory->setStatus(1);
 
+            $em->persist($user);
             $em->persist($pointHistory);
             $em->persist($taskHistory);
             $em->flush();
