a5566188549faf18b33de62ba45d07616df73f07
amy-zhao <Amy_zhao@researchpanelasia.com>
2016-07-26 11:34:12 +0800

    move help code from jili to wenwen file

--- src/Jili/ApiBundle/Controller/DefaultController.php
+++ src/Jili/ApiBundle/Controller/DefaultController.php
@@ -22,104 +22,13 @@ class DefaultController extends Controller
         }
         return $hash;
     }
-
-    /**
-     * @Route("/help", name="_default_help", requirements={"_scheme"="http"})
-     */
-    public function helpAction()
-    {
-        return $this->render('WenwenFrontendBundle:Help:index.html.twig');
-    }
-
-    /**
-     * @Route("/help/issue", name="_default_help_issue", requirements={"_scheme"="http"})
-     */
-    public function issueAction()
-    {
-        return $this->render('WenwenFrontendBundle:Help:issue.html.twig');
-    }
-
-    /**
-     * @Route("/help/newGuide", name="_default_help_newGuide", requirements={"_scheme"="http"})
-     */
-    public function guideAction()
-    {
-        return $this->render('WenwenFrontendBundle:Help:newGuide.html.twig');
-    }
-
-    /**
-     * @Route("/help/newGuide/detail", name="_default_help_newGuide_detail", requirements={"_scheme"="http"})
-     */
-    public function guideDetailAction()
-    {
-        return $this->render('WenwenFrontendBundle:Help:newGuideDetail.html.twig');
-    }
-
-    /**
-     * @Route("/help/feedback", name="_default_help_feedback", requirements={"_scheme"="http"})
-     */
-    public function feedbackAction()
-    {
-        return $this->render('WenwenFrontendBundle:Help:feedback.html.twig');
-    }
-
-    /**
-	 * @Route("/help/company", name="_default_help_company", requirements={"_scheme"="http"})
-	 */
-    public function companyAction()
-    {
-        return $this->render('WenwenFrontendBundle:Help:company.html.twig');
-    }
-
-    /**
-     * @Route("/help/ww", name="_default_help_ww", requirements={"_scheme"="http"})
-     */
-    public function wwAction()
-    {
-        return $this->render('WenwenFrontendBundle:Help:91ww.html.twig');
-    }
-
-    /**
-     * @Route("/help/regulations", name="_default_help_regulations", requirements={"_scheme"="http"})
-     */
-    public function regulationsAction()
-    {
-        return $this->render('WenwenFrontendBundle:Help:regulations.html.twig');
-    }
-
-    /**
-     * @Route("/help/map", name="_default_help_map", requirements={"_scheme"="http"})
-     */
-    public function mapAction()
-    {
-        return $this->render('WenwenFrontendBundle:Help:map.html.twig');
-    }
-
-    /**
-     * @Route("/help/cooperation", name="_default_help_cooperation", requirements={"_scheme"="http"})
-     */
-    public function cooperationAction()
-    {
-        return $this->render('WenwenFrontendBundle:Help:cooperation.html.twig');
-    }
-
-
+   
     /**
 	 * @Route("/error", name="_default_error", requirements={"_scheme"="http"})
 	 */
     public function errorAction()
     {
         return $this->render('WenwenFrontendBundle:Exception:index.html.twig');
-    }
-
-
-
-    /**
-     * @Route("/help/feedback/finished", name="_default_help_feedback_finished", requirements={"_scheme"="http"}, options={"expose"=true})
-     */
-    public function finishedAction()
-    {
-        return $this->render('WenwenFrontendBundle:Help:finished.html.twig');
     }
 
     /**
