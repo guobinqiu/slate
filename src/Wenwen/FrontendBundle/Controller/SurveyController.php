daa93660bf50dd1f11add68fb80cbca0f6a3c607
amy-zhao <Amy_zhao@researchpanelasia.com>
2016-07-27 12:02:53 +0800

    control home survey length

--- src/Wenwen/FrontendBundle/Controller/SurveyController.php
+++ src/Wenwen/FrontendBundle/Controller/SurveyController.php
@@ -55,7 +55,7 @@ class SurveyController extends Controller
             // test环境时不去访问SOP服务器，在circleCI上运行测试case时，访问SOP服务器会超时，导致测试运行极慢
             $surveyService->setDummy(true);
         }
-        $html_survey_list = $this->getOrderedHtmlSurveyList($user_id, 600);
+        $html_survey_list = $this->getOrderedHtmlSurveyList($user_id, 8);
 
         return $this->render('WenwenFrontendBundle:Survey:_sopSurveyListHome.html.twig', array('html_survey_list' => $html_survey_list));
     }
