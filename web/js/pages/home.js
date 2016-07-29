bc0a7c8d202d46118cd4562d33258d42a75f21ed
amy-zhao <Amy_zhao@researchpanelasia.com>
2016-07-29 14:18:53 +0800

    AB templates and track code

--- web/js/pages/home.js
+++ web/js/pages/home.js
@@ -29,25 +29,7 @@ $(function(){
         $('#mask, #newguideWrap').hide();
     }
 
-    //profile survey prompt popup
-    var surveyList = $('#surveyList');
-    var prop = $.cookie('prop');
-    var proCon = $('.proSurPop');
-    function shouldShow(){
-       var pp  = $.cookie('prop');
-       if (pp == undefined || pp == 1) {
-           return true;
-       } else {
-           return false;
-       }
-    }
-    surveyList.delegate('.proClose','click', function(){
-       $.cookie('prop', 0, { expires: 10000, path: '/' }); 
-       surveyList.find(proCon).hide();    
-    });
-    if(shouldShow()){
-       surveyList.find(proCon).show();
-    }else{
-       surveyList.find(proCon).hide();
-    }
+    //ABtest for profile prompt
+    $(document.body).attr("onload", "adhoc.init('ADHOC_0f2cb6b2-3772-4ee3-a06e-7248f4efdbdc')");
+      
 });
