c52d3b1a701cba622e30bb14a12de17c29bfb77f
amy-zhao <Amy_zhao@researchpanelasia.com>
2016-08-15 16:48:39 +0800

    change the way of import baidu JS code

---
+++ web/js/component/baiduStatistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?7cfb400697fc18ece65ee55e02f49026";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
