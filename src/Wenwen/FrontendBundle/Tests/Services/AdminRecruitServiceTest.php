b8801bf1c05915be361f702cadd117513c715f78
XiaoyiChai <xiaoyi.chai@d8aspring.com>
2016-11-15 14:25:10 +0800

    addRecruitMonthlyReport

--- src/Wenwen/FrontendBundle/Tests/Services/AdminRecruitServiceTest.php
+++ src/Wenwen/FrontendBundle/Tests/Services/AdminRecruitServiceTest.php
@@ -43,7 +43,7 @@ class AdminRecruitServiceTest extends WebTestCase
         $this->em->close();
     }
 
-    public function testGetSurveyPartnerList_ok(){
+    public function testGetDailyReport_ok(){
 
         $purger = new ORMPurger();
         $executor = new ORMExecutor($this->em, $purger);
@@ -128,10 +128,103 @@ class AdminRecruitServiceTest extends WebTestCase
         $from = (new \DateTime())->sub(new \DateInterval('P30D'))->setTime(0,0,0); 
         $to = (new \DateTime())->setTime(0,0,0);
 
-        $result = $this->adminRecruitService->getReport($from, $to);
+        $result = $this->adminRecruitService->getDailyReport($from, $to);
 
-        var_dump($result);
-        //$this->assertEquals(1, count($pagination), 'Should found one result.');
+        //var_dump($result);
+        $this->assertEquals(3, count($result['titles']), '应该有3个title');
+        $this->assertEquals(2, count($result['reports']), '应该有2个report');
+    }
+
+    public function testGetMonthlyReport_ok(){
+
+        $purger = new ORMPurger();
+        $executor = new ORMExecutor($this->em, $purger);
+        $executor->purge();
+
+        $registerCompleteDate = (new \DateTime())->sub(new \DateInterval('P80D')); // current time
+
+        $userProfile = new UserProfile();
+        $userProfile->setBirthday('1980-01-01'); // 36 岁
+        $userProfile->setSex(1); // 男性 
+
+        $user = new User();
+        $user->setEmail('test01@test.com');
+        $user->setRegisterCompleteDate($registerCompleteDate);
+        $user->setPoints(100);
+        $user->setRewardMultiple(1);
+        $user->setUserProfile($userProfile);
+
+        $userTrack = new UserTrack();
+        $userTrack->setSignInCount(1);
+        $userTrack->setRegisterRoute('XXX01');
+        $userTrack->setCurrentSignInAt($registerCompleteDate);
+        $userTrack->setCurrentSignInIp('xxx');
+        $userTrack->setUser($user);
+
+        $this->em->persist($user);
+        $this->em->persist($userTrack);
+        $this->em->flush();
+
+
+        $registerCompleteDate = (new \DateTime())->sub(new \DateInterval('P50D')); // current time
+
+        $userProfile = new UserProfile();
+        $userProfile->setBirthday('1980-01-01'); // 36 岁
+        $userProfile->setSex(1); // 男性 
+
+        $user = new User();
+        $user->setEmail('test02@test.com');
+        $user->setRegisterCompleteDate($registerCompleteDate);
+        $user->setPoints(100);
+        $user->setRewardMultiple(1);
+        $user->setUserProfile($userProfile);
+
+        $userTrack = new UserTrack();
+        $userTrack->setSignInCount(1);
+        $userTrack->setRegisterRoute('XXX01');
+        $userTrack->setCurrentSignInAt($registerCompleteDate);
+        $userTrack->setCurrentSignInIp('xxx');
+        $userTrack->setUser($user);
+
+        $this->em->persist($user);
+        $this->em->persist($userTrack);
+        $this->em->flush();
+
+        $registerCompleteDate = (new \DateTime())->sub(new \DateInterval('P40D')); // current time
+
+        $userProfile = new UserProfile();
+        $userProfile->setBirthday('1980-01-01'); // 36 岁
+        $userProfile->setSex(1); // 男性 
+
+        $user = new User();
+        $user->setEmail('test03@test.com');
+        $user->setRegisterCompleteDate($registerCompleteDate);
+        $user->setPoints(100);
+        $user->setRewardMultiple(1);
+        $user->setUserProfile($userProfile);
+
+        $userTrack = new UserTrack();
+        $userTrack->setSignInCount(1);
+        $userTrack->setRegisterRoute('XXX02');
+        $userTrack->setCurrentSignInAt($registerCompleteDate);
+        $userTrack->setCurrentSignInIp('xxx');
+        $userTrack->setUser($user);
+
+        $this->em->persist($user);
+        $this->em->persist($userTrack);
+        $this->em->flush();
+
+
+
+
+        //$from = (new \DateTime())->sub(new \DateInterval('P30D'))->setTime(0,0,0); 
+        //$to = (new \DateTime())->setTime(0,0,0);
+
+        $result = $this->adminRecruitService->getMonthlyReport();
+
+        //var_dump($result);
+        $this->assertEquals(3, count($result['titles']), '应该有3个title');
+        $this->assertEquals(3, count($result['reports']), '应该有2个report');
     }
 
 
