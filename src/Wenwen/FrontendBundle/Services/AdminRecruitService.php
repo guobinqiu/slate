6199f1cc39058b70971ea9a58186c934a2428e73
XiaoyiChai <xiaoyi.chai@d8aspring.com>
2016-11-15 15:09:07 +0800

    addRecruitMonthlyReport

--- src/Wenwen/FrontendBundle/Services/AdminRecruitService.php
+++ src/Wenwen/FrontendBundle/Services/AdminRecruitService.php
@@ -4,9 +4,11 @@ namespace Wenwen\FrontendBundle\Services;
 
 use Doctrine\ORM\EntityManager;
 use Psr\Log\LoggerInterface;
+use Predis\Client;
+use Wenwen\FrontendBundle\ServiceDependency\CacheKeys;
 
 /**
- * AdminSurvey
+ * 招募的统计信息
  */
 class AdminRecruitService
 {
@@ -16,17 +18,27 @@ class AdminRecruitService
 
     private $parameterService;
 
+    private $redis;
+
 
     public function __construct(LoggerInterface $logger,
                                 EntityManager $em,
-                                ParameterService $parameterService)
+                                ParameterService $parameterService,
+                                Client $redis)
     {
         $this->logger = $logger;
         $this->em = $em;
         $this->parameterService = $parameterService;
+        $this->redis = $redis;
     }
 
     public function getDailyReport($from = null, $to = null){
+        $val = $this->redis->get(CacheKeys::ADMIN_RECRUIT_REPORT_DAILY);
+        if(!empty($val)){
+            $this->logger->debug(__METHOD__ . ' Found daily report in redis. ' . $val);
+            return json_decode($val, true);
+        }
+
         if(empty($from)){
             $from = (new \DateTime())->sub(new \DateInterval('P30D'))->setTime(0,0,0); 
         }
@@ -86,11 +98,20 @@ class AdminRecruitService
         $return = array();
         $return['reports'] = $reports;
         $return['titles'] = $recruitRouteTitles;
-        
+
+        $this->redis->set(CacheKeys::ADMIN_RECRUIT_REPORT_DAILY, json_encode($return));
+        $this->redis->expire(CacheKeys::ADMIN_RECRUIT_REPORT_DAILY, CacheKeys::ADMIN_RECRUIT_REPORT_DAILY_TIMEOUT);
+        $this->logger->debug(__METHOD__ . ' Found monthly report in db. ' . json_encode($return));
         return $return;
     }
 
     public function getMonthlyReport($from = null, $to = null){
+        $val = $this->redis->get(CacheKeys::ADMIN_RECRUIT_REPORT_MONTHLY);
+        if(!empty($val)){
+            $this->logger->debug(__METHOD__ . ' Found monthly report in redis. ' . $val);
+            return json_decode($val, true);
+        }
+
         if(empty($from)){
             $from = (new \DateTime())->sub(new \DateInterval('P6M'))->setTime(0,0,0); 
         }
@@ -98,16 +119,10 @@ class AdminRecruitService
             $to = (new \DateTime())->setTime(0,0,0);
         }
 
-        $this->logger->debug('user_track from=' . $from->format('Y-m-d 00:00:00'));
-        $this->logger->debug('user_track to=' . $to->format('Y-m-d 00:00:00'));
-
         $results = $this->em->getRepository('WenwenFrontendBundle:User')->getRecruitRouteMonthlyCount($from, $to);
-
-        
 
         $recruitRouteTitles = array();
         $recruitRouteTitles['total'] = 0;
-
 
         // 补全所有出现的recruitRoute名称
         foreach($results as $result){
@@ -153,7 +168,11 @@ class AdminRecruitService
         $return = array();
         $return['reports'] = $reports;
         $return['titles'] = $recruitRouteTitles;
-        
+
+        $this->redis->set(CacheKeys::ADMIN_RECRUIT_REPORT_MONTHLY, json_encode($return));
+        $this->redis->expire(CacheKeys::ADMIN_RECRUIT_REPORT_MONTHLY, CacheKeys::ADMIN_RECRUIT_REPORT_MONTHLY_TIMEOUT);
+
+        $this->logger->debug(__METHOD__ . ' Found monthly report in db. ' . json_encode($return));
         return $return;
     }
 
