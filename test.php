4bd2d20828006773bd189b1e9c984b53669b2073
Guobin <guobin.qiu@d8aspring.com>
2016-11-24 17:55:31 +0800

    remove

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$nick = 'hi中国';
-echo PHP_EOL . substr($nick, 0, 3);
-echo PHP_EOL . mb_substr($nick, 0, 3, 'utf8');
-echo PHP_EOL . mb_substr($nick, 0, 3);
-echo PHP_EOL . mb_substr($nick, 0, 3, 'gbk');
