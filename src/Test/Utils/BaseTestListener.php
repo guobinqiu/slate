e64d66c0624c84eecf5c7c7db937772f25fab96f
Guobin <guobin.qiu@d8aspring.com>
2017-01-16 11:59:57 +0800

    gmo

--- src/Test/Utils/BaseTestListener.php
+++ src/Test/Utils/BaseTestListener.php
@@ -5,16 +5,10 @@ class BaseTestListener extends \PHPUnit_Framework_BaseTestListener
 {
     public function endTest(\PHPUnit_Framework_Test $test, $time)
     {
-        try {
-            printf("Test '%s' ended.\nTotal time %s s.\tTest time %s s.\n",
-                str_pad($test->toString(), 50),
-                number_format($test->getTestResultObject()->time(), 3),
-                number_format($time, 3)
-            );
-        } catch(\Exception $e) {
-            echo '-------' . $test->toString();
-            echo '-------' . $e->getMessage();
-        }
-
+        printf("Test '%s' ended.\nTotal time %s s.\tTest time %s s.\n",
+            str_pad($test->toString(), 50),
+            number_format($test->getTestResultObject()->time(), 3),
+            number_format($time, 3)
+        );
     }
 }
