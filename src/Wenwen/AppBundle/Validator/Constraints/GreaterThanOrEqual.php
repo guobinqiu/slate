727f6a52738c9ee151d85800e5893b3c7200d5e6
Guobin <guobin.qiu@d8aspring.com>
2017-01-23 12:57:38 +0800

    gmo

--- src/Wenwen/AppBundle/Validator/Constraints/GreaterThanOrEqual.php
+++ src/Wenwen/AppBundle/Validator/Constraints/GreaterThanOrEqual.php
@@ -6,8 +6,17 @@ use Symfony\Component\Validator\Constraint;
 
 /**
  * @Annotation
+ *
+ * @api
  */
 class GreaterThanOrEqual extends Constraint
 {
     public $message = 'This value should be greater than or equal to {{ compared_value }}.';
+
+    public $value;
+
+    public function getDefaultOption()
+    {
+        return 'value';
+    }
 }
--- src/Wenwen/AppBundle/Validator/Constraints/GreaterThanOrEqualValidator.php
+++ src/Wenwen/AppBundle/Validator/Constraints/GreaterThanOrEqualValidator.php
@@ -7,12 +7,13 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 class GreaterThanOrEqualValidator extends ConstraintValidator
 {
+    /**
+     * {@inheritDoc}
+     */
     public function validate($value, Constraint $constraint)
     {
         if ($value < $constraint->value) {
-            $this->context->addViolation($constraint->message, array(
-                '{{ compared_value }}' => $constraint->value
-            ));
+            $this->context->addViolation($constraint->message, array('{{ compared_value }}' => $constraint->value));
         }
     }
 }
