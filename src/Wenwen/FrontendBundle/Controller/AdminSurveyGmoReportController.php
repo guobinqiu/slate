2314c3e66f692caf75ac3cfea94d0e28a1ef9110
XiaoyiChai <xiaoyi.chai@d8aspring.com>
2017-02-15 11:38:05 +0800

    addAdminSurveyGMOReportSurveyList

---
+++ src/Wenwen/FrontendBundle/Controller/AdminSurveyGmoReportController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Wenwen\FrontendBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Wenwen\FrontendBundle\Entity\SurveyGmoNonBusiness;
+use Wenwen\FrontendBundle\Form\SurveyGmoNonBusinessType;
+
+/**
+ * SurveyGmoNonBusiness controller.
+ *
+ * @Route("/admin/gmo/report")
+ */
+class AdminSurveyGmoReportController extends Controller
+{
+    /**
+     * Lists all SurveyGmo entities.
+     *
+     * @Route("/surveylist", name="admin_gmo_report_surveylist")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('WenwenFrontendBundle:SurveyGmo')->findBy(array(), array('createdAt' => 'DESC'));
+
+        return $this->render("WenwenFrontendBundle:admin:SurveyGmoReport/surveylist.html.twig", array(
+            'entities' => $entities,
+        ));
+    }
+
+}
