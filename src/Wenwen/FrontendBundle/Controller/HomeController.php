e6ed9de3a7b90569bef7d0f73351475e3a402db4
XiaoyiChai <xiaoyi.chai@d8aspring.com>
2017-02-20 10:59:55 +0800

    removeSurveyTop

--- src/Wenwen/FrontendBundle/Controller/HomeController.php
+++ src/Wenwen/FrontendBundle/Controller/HomeController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Wenwen\FrontendBundle\Entity\User;
+use JMS\JobQueueBundle\Entity\Job;
+use Wenwen\FrontendBundle\Entity\SurveyListJob;
 
 class HomeController extends BaseController
 {
@@ -26,12 +28,62 @@ class HomeController extends BaseController
             return $this->render('WenwenFrontendBundle:Home:index.html.twig');
         }
 
+        $user_id = $request->getSession()->get('uid');
+
         if ($session->has('referer')) {
             $url = $session->get('referer');
             $session->remove('referer');
             return $this->redirect($url);
         }
 
-        return $this->render('WenwenFrontendBundle:Home:home.html.twig');
+        // 根据Ip获取该用户的地区信息
+        $locationInfo = $this->getLocationInfoByClientIp($request);
+
+        // 处理ssi和sop的排序，排序列表里存的是一个个通过模板渲染出来的html片段，每种模板分别对应一类问卷
+        $surveyService = $this->get('app.survey_service');
+        $env = $this->container->get('kernel')->getEnvironment();
+        if (in_array($env, array('dev','test'))) {
+            // for dummy mode (won't access sop's server at dev or test mode)
+            // test环境时不去访问SOP服务器，在circleCI上运行测试case时，访问SOP服务器会超时，导致测试运行极慢
+            $surveyService->setDummy(true);
+        }
+        $html_survey_list = $surveyService->getOrderedHtmlSurveyList($user_id, $locationInfo);
+
+        $this->checkoutSurveyList($user_id);
+
+        return $this->render('WenwenFrontendBundle:Home:home.html.twig', array(
+            'html_survey_list' => $html_survey_list,
+        ));
+    }
+
+    private function checkoutSurveyList($userId)
+    {
+        $timeslot = SurveyListJob::getTimeslot(time());
+        $min = new \DateTime($timeslot['min']);
+        $max = new \DateTime($timeslot['max']);
+        $em = $this->getDoctrine()->getManager();
+        $surveyListJob = $em->getRepository('WenwenFrontendBundle:SurveyListJob')->getSurveyListJob($userId, $min, $max);
+        if ($surveyListJob == null) {
+            $surveyListJob = new SurveyListJob($userId);
+            $em->persist($surveyListJob);
+            $em->flush();
+
+            //gmo
+            $surveyGmoService = $this->get('app.survey_gmo_service');
+            $researches = $surveyGmoService->getSurveyList($userId);
+            foreach ($researches as $research) {
+                $survey = $surveyGmoService->createOrUpdateSurvey($research);
+                $surveyGmoService->createParticipationByUserId($userId, $survey->getId(), SurveyStatus::STATUS_TARGETED);
+            }
+
+            //sop
+            $args = array(
+                '--user_id=' . $userId,
+            );
+            $job = new Job('sop:checkout_survey_list', $args, true, '91wenwen_sop');
+            $job->setMaxRetries(3);
+            $em->persist($job);
+            $em->flush();
+        }
     }
 }
