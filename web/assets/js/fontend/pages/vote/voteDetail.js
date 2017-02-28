1c176d31263b3c21988a50bc137a6c10f811bcca
lillian <wlt_liting@163.com>
2017-02-28 11:13:39 +0800

    modify dir name

--- web/assets/js/fontend/pages/vote/voteDetail.js
+++
@@ -1,9 +0,0 @@
-/*-------------------
-引用jquery.js, common.js, textScroll.js
--------------------*/
-$(function(){
-	var progress = $('.progress');
-	for(var i = 0; i< progress.length; i++){
-		progress.eq(i).find('p').css('width', progress.eq(i).find('input').val());
-	}
-});
