15a39b940c94d88483184c3869d4818b281cf46c
lillian <wlt_liting@163.com>
2017-03-03 11:23:02 +0800

    delete unUse assets

--- web/js/component/baiduStatistics.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?0e6678f1436774e9380eec614860e3a6";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
