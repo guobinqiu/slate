a56865412b836e3e04e45fe678c4d8398a81b270
xiaoyi.chai <xiaoyi.chai@d8aspring.com>
2017-08-16 14:12:58 +0800

    add error handling

--- src/Wenwen/AppBundle/Command/SsiPointRewardCommand.php
+++ src/Wenwen/AppBundle/Command/SsiPointRewardCommand.php
@@ -35,128 +35,134 @@ class SsiPointRewardCommand extends ContainerAwareCommand
     {
         $output->writeln(date('Y-m-d H:i:s') . ' start ' . $this->getName());
 
-        $date = $input->getArgument('date');
-        $definitive = $input->getOption('definitive');
-        $resultNotification = $input->getOption('resultNotification');
+        try{
+            $date = $input->getArgument('date');
+            $definitive = $input->getOption('definitive');
+            $resultNotification = $input->getOption('resultNotification');
 
-        $logger = $this->getLogger();
-        $logger->info(__METHOD__ . ' START ' . $this->getName() . ' date=' . $date);
+            $logger = $this->getLogger();
+            $logger->info(__METHOD__ . ' START ' . $this->getName() . ' date=' . $date);
 
-        $client = new StatClient($this->getContainer()->getParameter('ssi_project_survey_code')['api_key']);
-        $iterator = $this->getContainer()->get('ssi_api.conversion_report_iterator');
-        $iterator->initialize($client, $date);
+            $client = new StatClient($this->getContainer()->getParameter('ssi_project_survey_code')['api_key']);
+            $iterator = $this->getContainer()->get('ssi_api.conversion_report_iterator');
+            $iterator->initialize($client, $date);
 
-        $em = $this->getContainer()->get('doctrine')->getManager();
-        $dbh = $em->getConnection();
+            $em = $this->getContainer()->get('doctrine')->getManager();
+            $dbh = $em->getConnection();
 
-        // flags
-        $successMessages = array();
-        $skipMessages = array();
-        $errorMessages = array();
+            // flags
+            $successMessages = array();
+            $skipMessages = array();
+            $errorMessages = array();
 
-        $ssiProjectConfig = $this->getContainer()->getParameter('ssi_project_survey');
+            $ssiProjectConfig = $this->getContainer()->getParameter('ssi_project_survey');
 
-        $rows = 0;
-        while ($row = $iterator->nextConversion()) {
-            $start = time();
-            $rows += 1;
+            $rows = 0;
+            while ($row = $iterator->nextConversion()) {
+                $start = time();
+                $rows += 1;
 
-            $ssiRespondentId = SsiRespondent::parseRespondentId($row['sub_id_5']);
-            $ssiRespondent = $em->getRepository('WenwenAppBundle:SsiRespondent')->findOneById($ssiRespondentId);
-            if (!$ssiRespondent) {
-                $msg = sprintf(' Skip reward, SsiRespondent (Id: %s) not exist. %s', $ssiRespondentId, json_encode($row));
-                $logger->warn(__METHOD__ . $msg);
-                array_push($skipMessages, date('Y-m-d H:i:s') . $msg);
-                continue;
-            }
-
-            $userId = $ssiRespondent->getUserId();
-            $user = $em->getRepository('WenwenFrontendBundle:User')->findOneById($userId);
-            if (!$user) {
-                $msg = sprintf(' Skip reward, User (Id: %s) not found.  %s', $userId, json_encode($row));
-                $logger->warn(__METHOD__ . $msg);
-                array_push($skipMessages, date('Y-m-d H:i:s') . $msg);
-                continue;
-            }
-
-            $dt = new \DateTime(
-                DateUtil::convertTimeZone($row['date_time'], self::REPORT_TIME_ZONE, self::REWARD_TIME_ZONE)
-            );
-
-            // check SsiProjectParticipationHistory exist
-            $records = $em->getRepository('WenwenAppBundle:SsiProjectParticipationHistory')->findBy(array (
-                'completedAt' => $dt,
-                'transactionId' => $row['transaction_id']
-            ));
-            if (count($records) > 0) {
-                $msg = sprintf(' Skip reward, already exist, skip transaction_id : %s.  %s', $row['transaction_id'], json_encode($row));
-                $logger->warn(__METHOD__ . $msg);
-                array_push($skipMessages, date('Y-m-d H:i:s') . $msg);
-                continue;
-            }
-
-            $dbh->beginTransaction();
-
-            try {
-                $history = $this->recordParticipationHistory($ssiRespondent, $row);
-
-                $pointService = $this->getContainer()->get('app.point_service');
-
-                // 给当前用户加积分
-                $pointService->addPoints(
-                    $user,
-                    $ssiProjectConfig['point'],
-                    CategoryType::SSI_COST,
-                    TaskType::SURVEY,
-                    sprintf('%s (%s)', $ssiProjectConfig['title'], $dt->format('Y-m-d')),
-                    $history
-                );
-
-                // 同时给邀请人加积分(10%)
-                $pointService->addPointsForInviter(
-                    $user,
-                    $ssiProjectConfig['point'] * 0.1,
-                    CategoryType::EVENT_INVITE_SURVEY,
-                    TaskType::RENTENTION,
-                    '您的好友' . $user->getNick() . '回答了一份SSI商业问卷',
-                    $history
-                );
-
-                // 给奖池注入积分(5%)
-                $injectPoints = intval($ssiProjectConfig['point'] * 0.05);
-                $this->getContainer()->get('app.prize_service')->addPointBalance($injectPoints);
-
-                if($definitive) {
-                    $dbh->commit();
-                    $msg = sprintf(' Commit   - rewarded point=%s user_id=%s %s', $ssiProjectConfig['point'], $userId, json_encode($row));
-                } else {
-                    $dbh->rollBack();
-                    $msg = sprintf(' RollBack - rewarded point=%s user_id=%s %s', $ssiProjectConfig['point'], $userId, json_encode($row));
+                $ssiRespondentId = SsiRespondent::parseRespondentId($row['sub_id_5']);
+                $ssiRespondent = $em->getRepository('WenwenAppBundle:SsiRespondent')->findOneById($ssiRespondentId);
+                if (!$ssiRespondent) {
+                    $msg = sprintf(' Skip reward, SsiRespondent (Id: %s) not exist. %s', $ssiRespondentId, json_encode($row));
+                    $logger->warn(__METHOD__ . $msg);
+                    array_push($skipMessages, date('Y-m-d H:i:s') . $msg);
+                    continue;
                 }
 
-                array_push($successMessages, date('Y-m-d H:i:s') . $msg);
-                $logger->info(__METHOD__ . $msg);
+                $userId = $ssiRespondent->getUserId();
+                $user = $em->getRepository('WenwenFrontendBundle:User')->findOneById($userId);
+                if (!$user) {
+                    $msg = sprintf(' Skip reward, User (Id: %s) not found.  %s', $userId, json_encode($row));
+                    $logger->warn(__METHOD__ . $msg);
+                    array_push($skipMessages, date('Y-m-d H:i:s') . $msg);
+                    continue;
+                }
 
-            } catch (\Exception $e) {
-                $msg = sprintf(' %s, %s', $e->getMessage(), json_encode($history));
-                $logger->error(__METHOD__ . $msg);
-                array_push($errorMessages, date('Y-m-d H:i:s') . $msg);
-                $dbh->rollBack();
+                $dt = new \DateTime(
+                    DateUtil::convertTimeZone($row['date_time'], self::REPORT_TIME_ZONE, self::REWARD_TIME_ZONE)
+                );
+
+                // check SsiProjectParticipationHistory exist
+                $records = $em->getRepository('WenwenAppBundle:SsiProjectParticipationHistory')->findBy(array (
+                    'completedAt' => $dt,
+                    'transactionId' => $row['transaction_id']
+                ));
+                if (count($records) > 0) {
+                    $msg = sprintf(' Skip reward, already exist, skip transaction_id : %s.  %s', $row['transaction_id'], json_encode($row));
+                    $logger->warn(__METHOD__ . $msg);
+                    array_push($skipMessages, date('Y-m-d H:i:s') . $msg);
+                    continue;
+                }
+
+                $dbh->beginTransaction();
+
+                try {
+                    $history = $this->recordParticipationHistory($ssiRespondent, $row);
+
+                    $pointService = $this->getContainer()->get('app.point_service');
+
+                    // 给当前用户加积分
+                    $pointService->addPoints(
+                        $user,
+                        $ssiProjectConfig['point'],
+                        CategoryType::SSI_COST,
+                        TaskType::SURVEY,
+                        sprintf('%s (%s)', $ssiProjectConfig['title'], $dt->format('Y-m-d')),
+                        $history
+                    );
+
+                    // 同时给邀请人加积分(10%)
+                    $pointService->addPointsForInviter(
+                        $user,
+                        $ssiProjectConfig['point'] * 0.1,
+                        CategoryType::EVENT_INVITE_SURVEY,
+                        TaskType::RENTENTION,
+                        '您的好友' . $user->getNick() . '回答了一份SSI商业问卷',
+                        $history
+                    );
+
+                    // 给奖池注入积分(5%)
+                    $injectPoints = intval($ssiProjectConfig['point'] * 0.05);
+                    $this->getContainer()->get('app.prize_service')->addPointBalance($injectPoints);
+
+                    if($definitive) {
+                        $dbh->commit();
+                        $msg = sprintf(' Commit   - rewarded point=%s user_id=%s %s', $ssiProjectConfig['point'], $userId, json_encode($row));
+                    } else {
+                        $dbh->rollBack();
+                        $msg = sprintf(' RollBack - rewarded point=%s user_id=%s %s', $ssiProjectConfig['point'], $userId, json_encode($row));
+                    }
+
+                    array_push($successMessages, date('Y-m-d H:i:s') . $msg);
+                    $logger->info(__METHOD__ . $msg);
+
+                } catch (\Exception $e) {
+                    $msg = sprintf(' %s, %s', $e->getMessage(), json_encode($history));
+                    $logger->error(__METHOD__ . $msg);
+                    array_push($errorMessages, date('Y-m-d H:i:s') . $msg);
+                    $dbh->rollBack();
+                }
+            } // end while
+
+            $logger->info(__METHOD__ . ' RESULT total=' . $rows . ' success=' . count($successMessages) . ' skip=' . count($skipMessages) . ' error=' . count($errorMessages));
+
+            if($resultNotification){
+                $log = $this->getLog($successMessages, $skipMessages, $errorMessages);
+                $subject = 'Report of SSI reward points for ' . $date;
+                $numSent = $this->sendLogEmail($log, $subject);
+                $logger->info(__METHOD__ . ' End   of notification. Email num sent: ' . $numSent . ' title=' . $subject);
+            } else {
+                $logger->info(__METHOD__ . ' End without notification.');
             }
-        } // end while
 
-        $logger->info(__METHOD__ . ' RESULT total=' . $rows . ' success=' . count($successMessages) . ' skip=' . count($skipMessages) . ' error=' . count($errorMessages));
+            $logger->info(__METHOD__ . ' END   ' . $this->getName() . ' date=' . $date);
 
-        if($resultNotification){
-            $log = $this->getLog($successMessages, $skipMessages, $errorMessages);
-            $subject = 'Report of SSI reward points for ' . $date;
-            $numSent = $this->sendLogEmail($log, $subject);
-            $logger->info(__METHOD__ . ' End   of notification. Email num sent: ' . $numSent . ' title=' . $subject);
-        } else {
-            $logger->info(__METHOD__ . ' End without notification.');
+        } catch (\Exception $e) {
+            $logger->error(__METHOD__ . ' ERROR: ' . $e->getMessage());
+            $output->writeln(date('Y-m-d H:i:s') . ' ERROR: ' . $e->getMessage());
         }
-
-        $logger->info(__METHOD__ . ' END   ' . $this->getName() . ' date=' . $date);
 
         $output->writeln(date('Y-m-d H:i:s') . ' end ' . $this->getName());
     }
